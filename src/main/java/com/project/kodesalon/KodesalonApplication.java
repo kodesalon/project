3c9f102577efcdb618cfaa2fea40c5daceb2a573
Beenie93 <seongbeen93@naver.com>
2021-05-02 22:45:31 +0900

    initial commit

---
+++ src/main/java/com/project/kodesalon/KodesalonApplication.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodesalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodesalonApplication.class, args);
+	}
+
+}
