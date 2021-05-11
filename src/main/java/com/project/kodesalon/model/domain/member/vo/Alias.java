80801dc4b8f0f1276dc858df0d38f08e027c12a2
sdrs6921 <sdrs6921@naver.com>
2021-05-11 19:43:20 +0900

    feat : #9 Alias VO 클래스 생성

---
+++ src/main/java/com/project/kodesalon/model/domain/member/vo/Alias.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon.model.domain.member.vo;
+
+public class Alias {
+    private String alias;
+
+    public Alias (String alias) {
+        this.alias = alias;
+    }
+
+    public String value() {
+        return null;
+    }
+}
