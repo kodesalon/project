6f64ad92897dca12dbd513303e82788926456103
sdrs6921 <sdrs6921@naver.com>
2021-05-13 15:47:40 +0900

    feat : #12 Email 클래스 생성

---
+++ src/main/java/com/project/kodesalon/model/domain/vo/Email.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon.model.domain.vo;
+
+public class Email {
+    private final String email;
+
+    public Email (final String email) {
+        this.email = email;
+    }
+
+    public String getValue() {
+       return null;
+    }
+}
