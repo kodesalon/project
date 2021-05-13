5612380f88fc3c0747b45719c529ad0c02aa4136
sdrs6921 <sdrs6921@naver.com>
2021-05-13 16:21:01 +0900

    feat : #12 getValue 메서드 구현

--- src/main/java/com/project/kodesalon/model/domain/vo/Email.java
+++ src/main/java/com/project/kodesalon/model/domain/vo/Email.java
@@ -2,12 +2,13 @@ package com.project.kodesalon.model.domain.vo;
 
 public class Email {
     private final String email;
+    private static final String EMAIL_EXCEPTION_MESSAGE = "잘못된 이메일 형식입니다";
 
     public Email (final String email) {
         this.email = email;
     }
 
     public String getValue() {
-       return null;
+       return email;
     }
 }
