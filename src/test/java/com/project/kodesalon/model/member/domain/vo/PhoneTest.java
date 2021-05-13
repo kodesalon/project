b7f5617e3139eac27386240010b53c72e3177c41
sdrs6921 <sdrs6921@naver.com>
2021-05-13 19:36:30 +0900

    test : #10 PhoneTest 클래스 생성

---
+++ src/test/java/com/project/kodesalon/model/member/domain/vo/PhoneTest.java
@@ -0,0 +1,23 @@
+package com.project.kodesalon.model.member.domain.vo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneTest {
+
+    @Test
+    void validate_phone_init_value() {
+
+    }
+
+    @Test
+    void invalid_phone_throw_exception() {
+
+    }
+
+    @Test
+    void get_value_return_value() {
+
+    }
+}
