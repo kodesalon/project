543f677e7ef598607a4ce348dfaee8ea7d991459
sdrs6921 <sdrs6921@naver.com>
2021-05-13 14:04:20 +0900

    chore : 불필요한 import문 제거

--- src/test/java/com/project/kodesalon/model/domain/member/vo/AliasTest.java
+++ src/test/java/com/project/kodesalon/model/domain/member/vo/AliasTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.then;
 
