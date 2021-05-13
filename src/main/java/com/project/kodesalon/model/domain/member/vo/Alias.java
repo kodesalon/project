63ae0735677deebd3bedd1a5617a62e19929a95e
sdrs6921 <sdrs6921@naver.com>
2021-05-13 13:55:39 +0900

    chore : #9 Pattern 객체 상수화

--- src/main/java/com/project/kodesalon/model/domain/member/vo/Alias.java
+++ src/main/java/com/project/kodesalon/model/domain/member/vo/Alias.java
@@ -1,10 +1,12 @@
 package com.project.kodesalon.model.domain.member.vo;
 
 import java.util.IllegalFormatException;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Alias {
-    private static final String ALIAS_PATTERN = "^[a-zA-Z]{1}[a-zA-Z0-9]{4,11}$";
+    private static final String ALIAS_REGEX= "^[a-zA-Z]{1}[a-zA-Z0-9]{4,11}$";
+    private static final Pattern ALIAS_PATTERN = Pattern.compile(ALIAS_REGEX);
     private static final String INVALID_ALIAS_EXCEPTION_MESSAGE = "잘못된 Alias 형식입니다.";
 
     private final String alias;
@@ -18,7 +20,7 @@ public class Alias {
     }
 
     private String validateAlias(final String alias) {
-        if (Pattern.matches(ALIAS_PATTERN, alias)) {
+        if (ALIAS_PATTERN.matcher(alias).matches()) {
             return alias;
         }
 
