b5e391aac610b0f700c238d7ae090f30f41c4445
sdrs6921 <sdrs6921@naver.com>
2021-05-13 13:41:05 +0900

    chore : #9 한글에 관한 테스트 추가

--- src/test/java/com/project/kodesalon/model/domain/member/vo/AliasTest.java
+++ src/test/java/com/project/kodesalon/model/domain/member/vo/AliasTest.java
@@ -21,6 +21,7 @@ class AliasTest {
     private static final String NOT_START_WITH_ALPHABET = "1234";
     private static final String INCLUDE_SPECIAL_SYMBOL = "a_______";
     private static final String INVALID_ALIAS_EXCEPTION_MESSAGE = "잘못된 Alias 형식입니다.";
+    private static final String INCLUDE_NON_ALPHABET = "한글Alias";
 
     private Alias alias;
 
@@ -41,7 +42,8 @@ class AliasTest {
 
     @ParameterizedTest
     @ValueSource(strings = {UNDER_LENGTH, OVER_LENGTH,
-            INCLUDE_BLANK, NOT_START_WITH_ALPHABET, INCLUDE_SPECIAL_SYMBOL})
+            INCLUDE_BLANK, NOT_START_WITH_ALPHABET,
+            INCLUDE_SPECIAL_SYMBOL, INCLUDE_NON_ALPHABET})
     @DisplayName("alias에 타당한 문자열 포맷이 아니면 예외를 리턴합니다.")
     void invalidateFormatAlias_throw_exception(String invalidFormat) {
         //then
