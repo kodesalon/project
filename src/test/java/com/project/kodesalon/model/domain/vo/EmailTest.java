a9e7916b75b6e2e2303eed337fab42340fef8cf9
sdrs6921 <sdrs6921@naver.com>
2021-05-13 15:53:53 +0900

    test : #12 EmailTest 클래스 생성

---
+++ src/test/java/com/project/kodesalon/model/domain/vo/EmailTest.java
@@ -0,0 +1,24 @@
+package com.project.kodesalon.model.domain.vo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+    private static final String NORMAL_EMAIL = "email@email.com";
+
+    @Test
+    void valid_email_init_value() {
+
+    }
+
+    @Test
+    void invalid_email_throw_exception() {
+
+    }
+
+    @Test
+    void getValue_return_email() {
+    }
+}
