60d30fc4b9817fe915b1201b206416d904d9636c
sdrs6921 <sdrs6921@naver.com>
2021-05-13 16:18:29 +0900

    test : #12 Email VO 테스트 코드 작성

--- src/test/java/com/project/kodesalon/model/domain/vo/EmailTest.java
+++ src/test/java/com/project/kodesalon/model/domain/vo/EmailTest.java
@@ -1,24 +1,37 @@
 package com.project.kodesalon.model.domain.vo;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.then;
 
 class EmailTest {
     private static final String NORMAL_EMAIL = "email@email.com";
+    private static final String NORMAL_EMAIL_INCLUDE_NUMBER = "email1234@email.com";
+    private static final String NO_AT_EMAIL = "email1234.com";
+    private static final String NO_DOT_EMAIL = "email1234@emailcom";
+    private static final String NON_ALPHABET_EMAIL = "한국어1234@email.com";
+    private static final String EMAIL_EXCEPTION_MESSAGE = "잘못된 이메일 형식입니다";
 
-    @Test
-    void valid_email_init_value() {
+    @ParameterizedTest
+    @ValueSource(strings = {NORMAL_EMAIL, NORMAL_EMAIL_INCLUDE_NUMBER})
+    @DisplayName("getValue 메서드를 호출하면 이메일의 값을 리턴합니다.")
+    void get_value_return_value(String value) {
+        //given
+        Email email = new Email(value);
 
+        //then
+        then(email.getValue()).isEqualTo(value);
     }
 
-    @Test
-    void invalid_email_throw_exception() {
-
-    }
-
-    @Test
-    void getValue_return_email() {
+    @ParameterizedTest
+    @ValueSource(strings = {NO_AT_EMAIL, NO_DOT_EMAIL, NON_ALPHABET_EMAIL})
+    @DisplayName("유효하지 않은 이메일은 예외를 발생시킵니다")
+    void invalid_email_throw_exception(String value) {
+        //then
+        assertThatThrownBy(() -> new Email(value)).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining(EMAIL_EXCEPTION_MESSAGE);
     }
 }
