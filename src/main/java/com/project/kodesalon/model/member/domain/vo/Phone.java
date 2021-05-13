48257db08f71596b3e490a76570af7003d01372c
sdrs6921 <sdrs6921@naver.com>
2021-05-13 19:28:57 +0900

    feature : #10 핸드폰 번호 VO 클래스 생성

---
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Phone.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon.model.member.domain.vo;
+
+public class Phone {
+    private String phone;
+
+    public Phone (String phone) {
+        this.phone = phone;
+    }
+
+    public String getValue() {
+        return null;
+    }
+}
