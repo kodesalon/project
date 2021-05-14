d6253fe624896e8b5d592c9647bbf8e78d87e122
sdrs6921 <sdrs6921@naver.com>
2021-05-14 16:03:12 +0900

    test : #13 Name 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/member/domain/vo/NameTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/vo/NameTest.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 
 class NameTest {
+    private static final String NAME_EXCEPTION_MESSAGE= "잘못된 Name 형식입니다.";
+    private static final String VALID_NAME_LENGTH_TWO = "김씨";
+    private static final String VALID_NAME_LENGTH_SEVENTEEN = "박하늘별님구름햇님보다사랑스러우리";
+    private static final String INVALID_NAME_LENGTH_ONE = "김";
+    private static final String INVALID_NAME_LENGTH_EIGHTEEN = "박하늘별님구름햇님보다사랑스러우리님";
+    private static final String INVALID_NAME_INCLUDE_BLANK = "엄 이";
+    private static final String INVALID_NAME_INCLUDE_SPECIAL_SYMBOL = "엄~";
+    private static final String INVALID_NAME_INCLUDE_NON_KOREAN = "abc";
 
     @ParameterizedTest
     @ValueSource(strings = {})
