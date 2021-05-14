16521da7fda579190e9dd10983a09467d20efbba
sdrs6921 <sdrs6921@naver.com>
2021-05-14 16:09:39 +0900

    test : #13 유효하지 않은 이름 테스트 구현

--- src/test/java/com/project/kodesalon/model/member/domain/vo/NameTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/vo/NameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.then;
 
 
@@ -33,7 +34,9 @@ class NameTest {
             INVALID_NAME_INCLUDE_BLANK, INVALID_NAME_INCLUDE_SPECIAL_SYMBOL,
             INVALID_NAME_INCLUDE_NON_KOREAN})
     @DisplayName("유효하지 않은 이름은 예외를 발생시킵니다.")
-    void invalid_name_throw_exception() {
-
+    void invalid_name_throw_exception(String invalidName) {
+        //then
+        assertThatThrownBy(() -> new Name(invalidName)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NAME_EXCEPTION_MESSAGE);
     }
 }
