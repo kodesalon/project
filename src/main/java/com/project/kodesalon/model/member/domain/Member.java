f4201ff9e1186335c275eac16260d75b09e287ac
sdrs6921 <sdrs6921@naver.com>
2021-05-14 22:14:33 +0900

    feat : #16 Member 객체 속성값 get 메서드 구현

--- src/main/java/com/project/kodesalon/model/member/domain/Member.java
+++ src/main/java/com/project/kodesalon/model/member/domain/Member.java
@@ -3,10 +3,13 @@ package com.project.kodesalon.model.member.domain;
 import com.project.kodesalon.model.member.domain.vo.Alias;
 import com.project.kodesalon.model.member.domain.vo.Email;
 import com.project.kodesalon.model.member.domain.vo.Phone;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Immutable;
 
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +23,26 @@ public class Member {
 
     @Embedded
     private Phone phone;
+
+    public Member(String alias, String email, String phone) {
+        this.alias = new Alias(alias);
+        this.email = new Email(email);
+        this.phone = new Phone(phone);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAlias() {
+        return alias.getValue();
+    }
+
+    public String getEmail() {
+        return email.getValue();
+    }
+
+    public String getPhone() {
+        return phone.getValue();
+    }
 }
--- src/main/java/com/project/kodesalon/model/member/domain/vo/Alias.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Alias.java
@@ -1,5 +1,7 @@
 package com.project.kodesalon.model.member.domain.vo;
 
+import org.hibernate.annotations.Immutable;
+
 import javax.persistence.*;
 import java.util.regex.Pattern;
 
@@ -12,7 +14,7 @@ public class Alias {
     private static final String INVALID_ALIAS_EXCEPTION_MESSAGE = "잘못된 Alias 형식입니다.";
 
     private final String alias;
-
+    
     public Alias (final String alias) {
         if (!ALIAS_PATTERN.matcher(alias).matches()) {
             throw new IllegalArgumentException(INVALID_ALIAS_EXCEPTION_MESSAGE);
