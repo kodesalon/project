f2f1309590bd81ec41d15d088abb2196c5c52d75
sdrs6921 <sdrs6921@naver.com>
2021-05-14 16:15:31 +0900

    feat : #13 유효하지 않은 이름 검사

--- src/main/java/com/project/kodesalon/model/member/domain/vo/Name.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Name.java
@@ -1,9 +1,19 @@
 package com.project.kodesalon.model.member.domain.vo;
 
+import javax.print.attribute.standard.MediaSize;
+import java.util.regex.Pattern;
+
 public class Name {
     private final String name;
+    private static final String NAME_EXCEPTION_MESSAGE= "잘못된 Name 형식입니다.";
+    private static final String NAME_REGEX = "^[가-힣]{2,17}";
+    private static final Pattern NAME_PATTERN = Pattern.compile(NAME_REGEX);
 
     public Name (final String name) {
+        if (!NAME_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException(NAME_EXCEPTION_MESSAGE);
+        }
+
         this.name = name;
     }
 
