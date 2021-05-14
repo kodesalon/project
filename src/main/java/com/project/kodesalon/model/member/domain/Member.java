59beefe46ce042b3ceada55d73b4b26dbaf93aa8
sdrs6921 <sdrs6921@naver.com>
2021-05-14 19:05:01 +0900

    feat : #16 local 데이터베이스 jpa 환경 설정

---
+++ src/main/java/com/project/kodesalon/model/member/domain/Member.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon.model.member.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
