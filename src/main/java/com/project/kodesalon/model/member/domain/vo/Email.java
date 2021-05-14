5da51cae1ef8c3612ad87ffb52adac82e79b76a9
sdrs6921 <sdrs6921@naver.com>
2021-05-14 19:38:30 +0900

    feat : #16 Meber 클래스와 Email VO 매핑

--- src/main/java/com/project/kodesalon/model/member/domain/vo/Email.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Email.java
@@ -1,7 +1,11 @@
 package com.project.kodesalon.model.member.domain.vo;
 
+import javax.persistence.*;
 import java.util.regex.Pattern;
 
+@Embeddable
+@AttributeOverride(name = "email", column = @Column(name = "email"))
+@Access(AccessType.FIELD)
 public class Email {
     private static final String EMAIL_EXCEPTION_MESSAGE = "잘못된 이메일 형식입니다";
     private static final String EMAIL_REGEX = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
