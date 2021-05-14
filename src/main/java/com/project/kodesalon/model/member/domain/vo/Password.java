223fef55bed4c1c2a7c9fbe77fc478433d23a927
sdrs6921 <sdrs6921@naver.com>
2021-05-14 13:21:37 +0900

    feat : #11 비밀번호 클래스 생성

---
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Password.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon.model.member.domain.vo;
+
+public class Password {
+    private final String password;
+
+    public Password(final String password) {
+        this.password = password;
+    }
+
+    public String getValue() {
+        return null;
+    }
+}
