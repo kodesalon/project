380a9d31765905417087ea46b3de21aa0c99e098
sdrs6921 <sdrs6921@naver.com>
2021-05-14 13:45:26 +0900

    test : #11 테스트 케이스 생성

--- src/test/java/com/project/kodesalon/model/member/domain/vo/PasswordTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/vo/PasswordTest.java
@@ -9,6 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordTest {
     private static final String PASSWORD_EXCEPTION_MESSAGE = "";
+    private static final String VALID_PASSWORD_LENGTH_EIGHT = "!!Pass12";
+    private static final String VALID_PASSWORD_LENGTH_SIXTEEN = "!!Password123456";
+    private static final String INVALID_PASSWORD_LENGTH_SEVEN = "!pass12";
+    private static final String INVALID_PASSWORD_LENGTH_SEVENTEEN = "!!Password1234567";
+    private static final String INVALID_PASSWORD_NON_INCLUDE_SPECIAL_SYMBOL = "Password12";
+    private static final String INVALID_PASSWORD_NON_INCLUDE_NUMBER = "!!Password";
+    private static final String INVALID_PASSWORD_NON_INCLUDE_UPPER_CASE = "!!password12";
+    private static final String INVALID_PASSWORD_NON_INCLUDE_LOWER_CASE = "!!PASSWORD12";
+    private static final String INVALID_PASSWORD_INCLUDE_NON_ALPHABET = "!비밀!pass1234";
+
     @ParameterizedTest
     @ValueSource(strings = {})
     @DisplayName("유효한 비밀번호는 값을 초기화 합니다.")
