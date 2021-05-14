799b98379b000a1fb2f773afa1744f5b47110aa3
sdrs6921 <sdrs6921@naver.com>
2021-05-14 15:47:55 +0900

    feat : #13 Nmae VO 클래스 생성

---
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Name.java
@@ -0,0 +1,13 @@
+package com.project.kodesalon.model.member.domain.vo;
+
+public class Name {
+    private final String name;
+
+    public Name (final String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return null;
+    }
+}
