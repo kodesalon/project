23ce41101bea712efcfe8e385f392abdecf497f6
sdrs6921 <sdrs6921@naver.com>
2021-05-17 13:21:54 +0900

    refactor : #16 Alias final 키워드 제거
    jpa에서는 빈 생성자로 객체에 접근하기 때문에
    final 키워드를 제거하고 private 기본 생성자 생성

--- src/main/java/com/project/kodesalon/model/member/domain/vo/Alias.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Alias.java
@@ -1,24 +1,30 @@
 package com.project.kodesalon.model.member.domain.vo;
 
-import org.hibernate.annotations.Immutable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.util.regex.Pattern;
 
 @Embeddable
-@AttributeOverride(name = "alias", column = @Column(name = "alias"))
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Access(AccessType.FIELD)
 public class Alias {
-    private static final String ALIAS_REGEX= "^[a-zA-Z]{1}[a-zA-Z0-9]{4,11}$";
+    private static final String ALIAS_REGEX = "^[a-zA-Z]{1}[a-zA-Z0-9]{4,15}$";
     private static final Pattern ALIAS_PATTERN = Pattern.compile(ALIAS_REGEX);
     private static final String INVALID_ALIAS_EXCEPTION_MESSAGE = "잘못된 Alias 형식입니다.";
 
-    private final String alias;
-    
-    public Alias (final String alias) {
+    @Column(name = "alias", nullable = false, unique = true, length = 15)
+    private String alias;
+
+    public Alias(final String alias) {
         if (!ALIAS_PATTERN.matcher(alias).matches()) {
             throw new IllegalArgumentException(INVALID_ALIAS_EXCEPTION_MESSAGE);
         }
+
         this.alias = alias;
     }
 
