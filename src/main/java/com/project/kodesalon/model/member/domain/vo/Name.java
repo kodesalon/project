7331f7df7626a28a911a9aacf87e262851bf8dc2
sdrs6921 <sdrs6921@naver.com>
2021-05-18 12:59:26 +0900

    feat : #16 Name 클래스 Jpa 설정 변경

--- src/main/java/com/project/kodesalon/model/member/domain/vo/Name.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Name.java
@@ -1,14 +1,21 @@
 package com.project.kodesalon.model.member.domain.vo;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.util.regex.Pattern;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Name {
-    private final String name;
     private static final String NAME_EXCEPTION_MESSAGE= "잘못된 Name 형식입니다.";
     private static final String NAME_REGEX = "^[가-힣]{2,17}";
     private static final Pattern NAME_PATTERN = Pattern.compile(NAME_REGEX);
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
 
     public Name (final String name) {
         if (!NAME_PATTERN.matcher(name).matches()) {
