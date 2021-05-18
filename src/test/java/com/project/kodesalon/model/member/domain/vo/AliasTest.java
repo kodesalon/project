18278c9561f9f0756d95766cbf8f81c20fb537fb
sdrs6921 <sdrs6921@naver.com>
2021-05-18 18:31:59 +0900

    refactor : #16 불필요한 import문 제거

--- src/test/java/com/project/kodesalon/model/member/domain/vo/AliasTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/vo/AliasTest.java
@@ -1,6 +1,5 @@
 package com.project.kodesalon.model.member.domain.vo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
