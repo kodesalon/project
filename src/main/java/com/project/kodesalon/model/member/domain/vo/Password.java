ca47bc33b12f547bb9738379b856f0606ca7ecff
sdrs6921 <sdrs6921@naver.com>
2021-05-18 13:01:12 +0900

    feat : #16 password 클래스 jpa 속성 변경

--- src/main/java/com/project/kodesalon/model/member/domain/vo/Password.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Password.java
@@ -1,15 +1,21 @@
 package com.project.kodesalon.model.member.domain.vo;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.util.regex.Pattern;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Password {
     private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
     private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
     private static final String PASSWORD_EXCEPTION_MESSAGE = "잘못된 Password 형식입니다.";
 
-    private final String password;
+    @Column(name = "password", nullable = false, length = 30)
+    private String password;
 
     public Password(final String password) {
         if (!PASSWORD_PATTERN.matcher(password).matches()) {
