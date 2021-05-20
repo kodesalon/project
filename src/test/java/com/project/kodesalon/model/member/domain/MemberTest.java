6ee32dedd6e10410fda4f245f6f43f4c25a0a4ac
sdrs6921 <sdrs6921@naver.com>
2021-05-20 16:27:36 +0900

    feat : #20 멤버의 비밀번호 일치 여부 확인하는 메서드 테스트

--- src/test/java/com/project/kodesalon/model/member/domain/MemberTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/MemberTest.java
@@ -4,6 +4,8 @@ package com.project.kodesalon.model.member.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
@@ -13,6 +15,12 @@ class MemberTest {
     private static final String NAME = "엄희상";
     private static final String EMAIL = "email@email.com";
     private static final String PHONE = "010-1234-4444";
+    private static final String CSV_DELIMITER = ",";
+    private static final String INCORRECT_PASSWORD = "Password!!1234";
+    private static final String TRUE = "true";
+    private static final String FALSE = "false";
+    private static final String INCORRECT_PASSWORD_TEST_CASE = INCORRECT_PASSWORD + CSV_DELIMITER + TRUE;
+    private static final String CORRECT_PASSWORD_TEST_CASE = PASSWORD + CSV_DELIMITER + FALSE;
 
     private Member member;
 
@@ -49,5 +57,12 @@ class MemberTest {
     @DisplayName(("Member 객체를 생성하면 Phone을 초기화 합니다."))
     void create_phone() {
         then(member.getPhone()).isEqualTo(PHONE);
+    }
+
+    @ParameterizedTest
+    @CsvSource({INCORRECT_PASSWORD_TEST_CASE, CORRECT_PASSWORD_TEST_CASE})
+    @DisplayName("Member의 비밀번호가 일치하지 않으면 true, 일치하면 false를 리턴합니다.")
+    void is_incorrect_password(String password, boolean expected) {
+        then(Member.isIncorrectPassword(password)).isEqualTo(expected);
     }
 }
