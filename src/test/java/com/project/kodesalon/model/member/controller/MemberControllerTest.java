b4b021d1a9c1b8d237bdc704c0864dbe8f6dd9a9
sdrs6921 <sdrs6921@naver.com>
2021-05-24 21:45:55 +0900

    test : #20 로그인 성공 Controller 테스트 코드 구현

---
+++ src/test/java/com/project/kodesalon/model/member/controller/MemberControllerTest.java
@@ -0,0 +1,77 @@
+package com.project.kodesalon.model.member.controller;
+
+import com.project.kodesalon.model.member.dto.LoginRequestDto;
+import com.project.kodesalon.model.member.dto.LoginResponseDto;
+import com.project.kodesalon.model.member.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith({RestDocumentationExtension.class,
+        SpringExtension.class, MockitoExtension.class})
+@SpringBootTest
+public class MemberControllerTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private MemberService memberService;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext,
+               RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("로그인이 성공하면 Alias, ID, Http Status 200를 Response 합니다.")
+    void login_controller_return_success_response() throws Exception {
+        LoginRequestDto loginRequestDto = new LoginRequestDto("alias", "Password1!!");
+
+        when(memberService.login(any()))
+                .thenReturn(new LoginResponseDto(HttpStatus.OK, 1L, "alias"));
+
+        this.mockMvc.perform(post("/api/v1/members/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("login" ,
+                        responseFields(
+                                fieldWithPath("member.id").description("member id field"),
+                                fieldWithPath("member.alias").description("member alias field"))));
+    }
+}
