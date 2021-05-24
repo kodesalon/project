3918b5eec2fa4701332a361de6c4d092835ed6db
Beenie93 <seongbeen93@naver.com>
2021-05-25 02:41:57 +0900

    feat: #14 제목 공백 또는 아무것도 입력하지 않을 경우 예외 발생 조건문 추가

--- src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
+++ src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
@@ -3,9 +3,15 @@ package com.project.kodesalon.model.board.domain.vo;
 import java.util.Objects;
 
 public class Title {
+    private static final String CHECK_TITLE_IS_BLANK = "제목에 공백 아닌 1자 이상의 문자를 입력하였는지 확인해주세요.";
+
     private final String title;
 
     public Title(final String title) {
+        if (title.isBlank()) {
+            throw new IllegalArgumentException(CHECK_TITLE_IS_BLANK);
+        }
+
         this.title = title;
     }
 
