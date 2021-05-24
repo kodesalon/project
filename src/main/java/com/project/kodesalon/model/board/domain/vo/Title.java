6d04b96a07852a4a555c783d74d6f8ab319e1387
Beenie93 <seongbeen93@naver.com>
2021-05-25 03:20:40 +0900

    feat: #14 제목의 길이가 30자를 초과할 경우의 예외 발생 조건문 추가

--- src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
+++ src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
@@ -2,17 +2,35 @@ package com.project.kodesalon.model.board.domain.vo;
 
 import java.util.Objects;
 
+import static java.lang.String.format;
+
 public class Title {
     private static final String CHECK_TITLE_IS_BLANK = "제목에 공백 아닌 1자 이상의 문자를 입력하였는지 확인해주세요.";
+    private static final String CHECK_TITLE_LENGTH = "제목 글자 수가 %d을 초과하였는지 확인해주세요.";
+    private static final int TITLE_LENGTH_MAX_BOUND = 30;
 
     private final String title;
 
     public Title(final String title) {
+        validate(title);
+        this.title = title;
+    }
+
+    private void validate(String title) {
+        checkBlank(title);
+        checkLength(title);
+    }
+
+    private void checkBlank(String title) {
         if (title.isBlank()) {
             throw new IllegalArgumentException(CHECK_TITLE_IS_BLANK);
         }
+    }
 
-        this.title = title;
+    private void checkLength(String title) {
+        if (title.length() > TITLE_LENGTH_MAX_BOUND) {
+            throw new IllegalArgumentException(format(CHECK_TITLE_LENGTH, TITLE_LENGTH_MAX_BOUND));
+        }
     }
 
     @Override
