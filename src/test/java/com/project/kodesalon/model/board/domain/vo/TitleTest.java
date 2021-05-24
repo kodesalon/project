7da2849ca6c57cc45caae3e23641cb448e303cc8
Beenie93 <seongbeen93@naver.com>
2021-05-25 02:39:38 +0900

    test: #14 제목 공백 또는 아무것도 입력하지 않을 경우 예외 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/board/domain/vo/TitleTest.java
+++ src/test/java/com/project/kodesalon/model/board/domain/vo/TitleTest.java
@@ -2,8 +2,11 @@ package com.project.kodesalon.model.board.domain.vo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class TitleTest {
 
@@ -13,5 +16,13 @@ public class TitleTest {
         String boardTitle = "test_board";
         Title title = new Title(boardTitle);
         assertThat(title).isEqualTo(new Title(boardTitle));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("공백 또는 아무것도 입력하지 않을 경우, 예외가 발생한다.")
+    public void check_blank(String blank) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Title(blank));
     }
 }
