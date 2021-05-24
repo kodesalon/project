0ae43ac21819722861ff6d5c76b77204329e3bf4
Beenie93 <seongbeen93@naver.com>
2021-05-25 03:20:23 +0900

    test: #14 제목의 길이가 30자를 초과할 경우의 예외 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/board/domain/vo/TitleTest.java
+++ src/test/java/com/project/kodesalon/model/board/domain/vo/TitleTest.java
@@ -17,12 +17,21 @@ public class TitleTest {
         Title title = new Title(boardTitle);
         assertThat(title).isEqualTo(new Title(boardTitle));
     }
-    
+
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
     @DisplayName("공백 또는 아무것도 입력하지 않을 경우, 예외가 발생한다.")
     public void check_blank(String blank) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Title(blank));
+                .isThrownBy(() -> new Title(blank))
+                .withMessageContaining("제목에 공백 아닌 1자 이상의 문자를 입력");
+    }
+
+    @Test
+    @DisplayName("제목의 길이가 30자를 초과할 경우, 예외가 발생한다.")
+    public void check_length() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Title("this title length is 31        "))
+                .withMessageContaining("제목 글자 수가 30을 초과");
     }
 }
