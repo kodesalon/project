003f62d7e5d0b304076b396283e1ffa3f08113f5
Beenie93 <seongbeen93@naver.com>
2021-05-25 02:24:14 +0900

    feat: #14 제목 객체 생성

---
+++ src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
@@ -0,0 +1,24 @@
+package com.project.kodesalon.model.board.domain.vo;
+
+import java.util.Objects;
+
+public class Title {
+    private final String title;
+
+    public Title(final String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Title title1 = (Title) o;
+        return Objects.equals(title, title1.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+}
