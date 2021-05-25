143dc0faf8db6ea6a7e47d099880104eb0bc27c3
sdrs6921 <sdrs6921@naver.com>
2021-05-25 21:44:36 +0900

    refactor : #16 Table 어노테이션 추가 및 unitqueConstraints 속성 사용

--- src/main/java/com/project/kodesalon/model/member/domain/Member.java
+++ src/main/java/com/project/kodesalon/model/member/domain/Member.java
@@ -12,9 +12,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @NoArgsConstructor
+@Table(name = "member", uniqueConstraints = {
+        @UniqueConstraint(
+                columnNames = {"alias"}
+        )})
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/project/kodesalon/model/member/domain/vo/Alias.java
+++ src/main/java/com/project/kodesalon/model/member/domain/vo/Alias.java
@@ -14,7 +14,7 @@ public class Alias {
     private static final Pattern ALIAS_PATTERN = Pattern.compile(ALIAS_REGEX);
     private static final String INVALID_ALIAS_EXCEPTION_MESSAGE = "Alias 는 영문으로 시작해야 하며 4자리 이상 15자리 이하의 영문 혹은 숫자가 포함되어야 합니다.";
 
-    @Column(name = "alias", nullable = false, unique = true, length = 15)
+    @Column(name = "alias", nullable = false, length = 15)
     private String alias;
 
     public Alias(final String alias) {
