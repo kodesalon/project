673c8424194dc54ffc88520b13d4f412a58ecfda
sdrs6921 <sdrs6921@naver.com>
2021-05-25 20:40:03 +0900

    feat : #20 GlobalExceptionHandler에 로그 기능 추가

--- src/main/java/com/project/kodesalon/common/GlobalExceptionHandler.java
+++ src/main/java/com/project/kodesalon/common/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.project.kodesalon.common;
 
 import com.project.kodesalon.model.member.exception.UnAuthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(UnAuthorizedException.class)
     protected ResponseEntity<ErrorResponse> handleUnAuthorizedException(UnAuthorizedException e) {
+        logger.info(e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 }
