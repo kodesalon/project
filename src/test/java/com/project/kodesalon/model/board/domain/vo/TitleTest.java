7ff165530f4876a17a0194f3d2794ed12c68352f
Beenie93 <seongbeen93@naver.com>
2021-05-27 01:06:46 +0900

    test: #14 제목의 값을 받환하는 함수의 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/board/domain/vo/TitleTest.java
+++ src/test/java/com/project/kodesalon/model/board/domain/vo/TitleTest.java
@@ -1,21 +1,27 @@
 package com.project.kodesalon.model.board.domain.vo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class TitleTest {
+    private String boardTitle;
+    private Title title;
+
+    @BeforeEach
+    void setUp() {
+        boardTitle = "test_board";
+        title = new Title(boardTitle);
+    }
 
     @Test
     @DisplayName("문자열을 입력받아 제목 객체를 생성한다.")
     public void create() {
-        String boardTitle = "test_board";
-        Title title = new Title(boardTitle);
         assertThat(title).isEqualTo(new Title(boardTitle));
     }
 
@@ -26,5 +32,12 @@ public class TitleTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Title(blank))
                 .withMessageContaining("제목에 공백 아닌 1자 이상의 문자를 입력");
+    }
+
+    @Test
+    @DisplayName("제목의 문자열 값을 반환한다.")
+    public void getValue() {
+        String value = title.getValue();
+        assertThat(value).isEqualTo(boardTitle);
     }
 }
