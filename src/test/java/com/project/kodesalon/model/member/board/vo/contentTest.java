24f78f5cf8cd862837f7a64a5867a0defb4d6b65
Beenie93 <seongbeen93@naver.com>
2021-05-27 02:11:23 +0900

    test: #15 내용 공백 또는 아무것도 입력하지 않을 경우 예외 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/member/board/vo/contentTest.java
+++ src/test/java/com/project/kodesalon/model/member/board/vo/contentTest.java
@@ -3,8 +3,11 @@ package com.project.kodesalon.model.member.board.vo;
 import com.project.kodesalon.model.board.domain.vo.Content;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class contentTest {
 
@@ -14,5 +17,14 @@ public class contentTest {
         String boardContent = "test content";
         Content content = new Content(boardContent);
         assertThat(content).isEqualTo(new Content(boardContent));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("공백 또는 아무것도 입력하지 않을 경우, 예외가 발생한다.")
+    public void check_blank(String blank) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Content(blank))
+                .withMessageContaining("내용에 공백 아닌 1자 이상의 문자를 입력");
     }
 }
