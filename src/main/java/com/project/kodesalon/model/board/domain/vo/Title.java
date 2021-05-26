b98e1eb9baf4fee0dc8bc2d9f5f8765d0e132092
Beenie93 <seongbeen93@naver.com>
2021-05-27 00:30:35 +0900

    refactor: #14 JPA @EqualsAndHashCode 설정으로 인한 equals(), hashCode() 함수 제거

--- src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
+++ src/main/java/com/project/kodesalon/model/board/domain/vo/Title.java
@@ -1,14 +1,15 @@
 package com.project.kodesalon.model.board.domain.vo;
 
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import java.util.Objects;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
 public class Title {
     private static final String CHECK_TITLE_IS_BLANK = "제목에 공백 아닌 1자 이상의 문자를 입력하였는지 확인해주세요.";
     private static final int TITLE_LENGTH_MAX_BOUND = 30;
@@ -29,18 +30,5 @@ public class Title {
         if (title == null || title.isBlank()) {
             throw new IllegalArgumentException(CHECK_TITLE_IS_BLANK);
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Title title1 = (Title) o;
-        return Objects.equals(title, title1.title);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title);
     }
 }
