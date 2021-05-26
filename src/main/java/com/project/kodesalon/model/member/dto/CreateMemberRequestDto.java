3ba59d5fa6110a1763be8d5ad9ab1f17baefe69d
sdrs6921 <sdrs6921@naver.com>
2021-05-26 16:30:30 +0900

    refactor : #18 createRequestMemberDto 이름으로 변경

--- src/main/java/com/project/kodesalon/model/member/dto/CreateMemberRequestDto.java
+++ src/main/java/com/project/kodesalon/model/member/dto/CreateMemberRequestDto.java
@@ -5,14 +5,14 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-public class CreateMemberDto {
+public class CreateMemberRequestDto {
     private String alias;
     private String password;
     private String name;
     private String email;
     private String phone;
 
-    public CreateMemberDto(String alias, String password, String name, String email, String phone) {
+    public CreateMemberRequestDto(String alias, String password, String name, String email, String phone) {
         this.alias = alias;
         this.password = password;
         this.name = name;
--- src/test/java/com/project/kodesalon/model/member/dto/CreateMemberRequestDtoTest.java
+++ src/test/java/com/project/kodesalon/model/member/dto/CreateMemberRequestDtoTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class CreateMemberDtoTest {
+public class CreateMemberRequestDtoTest {
     private static final String ALIAS = "alias";
     private static final String PASSWORD = "Password123!!";
     private static final String NAME = "이름";
     private static final String EMAIL = "eamil@email.com";
     private static final String PHONE = "010-1111-2222";
 
-    private CreateMemberDto createMemberDto;
+    private CreateMemberRequestDto createMemberRequestDto;
 
     @BeforeEach
     void setUp() {
-        createMemberDto = new CreateMemberDto(ALIAS,
+        createMemberRequestDto = new CreateMemberRequestDto(ALIAS,
                 PASSWORD, NAME, EMAIL, PHONE);
     }
 
@@ -26,11 +26,11 @@ public class CreateMemberDtoTest {
     @DisplayName("CreateMemberDto를 생성하면 필드를 초기화 합니다.")
     void create_create_member_dto_init_field() {
         assertAll(
-                () -> then(createMemberDto.getAlias()).isEqualTo(ALIAS),
-                () -> then(createMemberDto.getPassword()).isEqualTo(PASSWORD),
-                () -> then(createMemberDto.getName()).isEqualTo(NAME),
-                () -> then(createMemberDto.getEmail()).isEqualTo(EMAIL),
-                () -> then(createMemberDto.getPhone()).isEqualTo(PHONE)
+                () -> then(createMemberRequestDto.getAlias()).isEqualTo(ALIAS),
+                () -> then(createMemberRequestDto.getPassword()).isEqualTo(PASSWORD),
+                () -> then(createMemberRequestDto.getName()).isEqualTo(NAME),
+                () -> then(createMemberRequestDto.getEmail()).isEqualTo(EMAIL),
+                () -> then(createMemberRequestDto.getPhone()).isEqualTo(PHONE)
         );
     }
 }
