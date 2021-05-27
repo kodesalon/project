7b97a44694d7994973c09b737fb18d94c5367f4d
Beenie93 <seongbeen93@naver.com>
2021-05-27 19:00:09 +0900

    feat: #15 내용의 길이가 500자를 초과할 경우의 예외 함수 추가

--- src/main/java/com/project/kodesalon/model/board/domain/vo/Content.java
+++ src/main/java/com/project/kodesalon/model/board/domain/vo/Content.java
@@ -13,17 +13,33 @@ import javax.persistence.Lob;
 @EqualsAndHashCode
 public class Content {
     private static final String CHECK_CONTENT_IS_BLANK = "내용에 공백 아닌 1자 이상의 문자를 입력하였는지 확인해주세요.";
+    private static final String CHECK_CONTENT_LENGTH = "내용이 500자를 초과하는지 확인해주세요.";
+    private static final int CONTENT_LENGTH_BOUND_MAX = 500;
 
     @Lob
-    @Column(nullable = false)
+    @Column(nullable = false, length = CONTENT_LENGTH_BOUND_MAX)
     private String content;
 
     public Content(String content) {
+        validate(content);
+        this.content = content;
+    }
+
+    private void validate(String content) {
+        checkNullOrBlank(content);
+        checkLength(content);
+    }
+
+    private void checkNullOrBlank(String content) {
         if (content == null || content.isBlank()) {
             throw new IllegalArgumentException(CHECK_CONTENT_IS_BLANK);
         }
+    }
 
-        this.content = content;
+    private void checkLength(String content) {
+        if (content.length() > CONTENT_LENGTH_BOUND_MAX) {
+            throw new IllegalArgumentException(CHECK_CONTENT_LENGTH);
+        }
     }
 
     public String value() {
