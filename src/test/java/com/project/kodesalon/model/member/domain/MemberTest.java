d6dd49d40ab5d2280d35015f58c9e2a3a50fb916
sdrs6921 <sdrs6921@naver.com>
2021-05-28 18:20:43 +0900

    test : #18 inCorrectPassword 테스트 작성

--- src/test/java/com/project/kodesalon/model/member/domain/MemberTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/MemberTest.java
@@ -1,9 +1,13 @@
 package com.project.kodesalon.model.member.domain;
 
 
+import com.project.kodesalon.model.member.domain.vo.Password;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -14,6 +18,7 @@ class MemberTest {
     private static final String NAME = "엄희상";
     private static final String EMAIL = "email@email.com";
     private static final String PHONE = "010-1234-4444";
+    private static final String INVALID_PASSWORD = "Invalid123!!";
 
     private Member member;
 
@@ -32,5 +37,13 @@ class MemberTest {
                 () -> then(member.getEmail()).isEqualTo(EMAIL),
                 () -> then(member.getPhone()).isEqualTo(PHONE)
         );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {PASSWORD + ",false", INVALID_PASSWORD + ",true"})
+    @DisplayName("Member의 비밀번호와 일치하지 않는다면 true, 일치하면 false를 리턴합니다.")
+    void incorrect_password_return_true(String password, boolean expect) {
+        then(member.isIncorrectPassword(new Password(password)))
+                .isEqualTo(expect);
     }
 }
