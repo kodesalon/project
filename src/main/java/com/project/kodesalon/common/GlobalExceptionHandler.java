72af897d7f983e2c7ce3b8dcdf5353f9d89ba783
sdrs6921 <sdrs6921@naver.com>
2021-05-31 17:48:45 +0900

    refactor : #20 log @slf4j 어노테이션으로 변경

--- src/main/java/com/project/kodesalon/common/GlobalExceptionHandler.java
+++ src/main/java/com/project/kodesalon/common/GlobalExceptionHandler.java
@@ -1,20 +1,18 @@
 package com.project.kodesalon.common;
 
 import com.project.kodesalon.model.member.exception.UnAuthorizedException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @ExceptionHandler(UnAuthorizedException.class)
     protected ResponseEntity<ErrorResponse> handleUnAuthorizedException(UnAuthorizedException e) {
-        logger.info(e.getMessage());
+        log.info(e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 }
