ba012c4928f4f7dd777d199a818220b0b61c01f8
Beenie93 <seongbeen93@naver.com>
2021-06-01 04:02:25 +0900

    refactor: #21 멤버 변수 -> 정적 변수로 변환

--- src/test/java/com/project/kodesalon/model/board/domain/BoardTest.java
+++ src/test/java/com/project/kodesalon/model/board/domain/BoardTest.java
@@ -2,7 +2,6 @@ package com.project.kodesalon.model.board.domain;
 
 import com.project.kodesalon.model.board.domain.vo.Content;
 import com.project.kodesalon.model.board.domain.vo.Title;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,31 +11,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class BoardTest {
-    private Board board;
-    private final String title = "title";
-    private final String content = "content";
-    private final String writer =  "writer";
-    private final LocalDateTime createdDateTime = LocalDateTime.now();
-
-    @BeforeEach
-    void setUp() {
-        board = Board.builder()
-                .title(new Title(title))
-                .content(new Content(content))
-                .writer(writer)
-                .createdDateTime(createdDateTime)
-                .build();
-    }
+    public static final String TITLE = "title";
+    public static final String CONTENT = "content";
+    public static final String WRITER = "writer";
+    public static final LocalDateTime CREATED_DATE_TIME = LocalDateTime.parse("2021-06-01T23:59:59.999999");
+    public static final Board TEST_BOARD = Board.builder()
+            .title(new Title(TITLE))
+            .content(new Content(CONTENT))
+            .writer(WRITER)
+            .createdDateTime(CREATED_DATE_TIME)
+            .build();
 
     @Test
     @DisplayName("게시물의 제목, 내용, 작성자, 생성 시간, 삭제 여부를 반환한다.")
     public void getter() {
         assertAll(
-                () -> assertThat(board.getTitle()).isEqualTo(title),
-                () -> assertThat(board.getContent()).isEqualTo(content),
-                () -> assertThat(board.getWriter()).isEqualTo(writer),
-                () -> assertThat(board.getCreatedDateTime()).isEqualTo(createdDateTime),
-                () -> assertThat(board.isDeleted()).isFalse()
+                () -> assertThat(TEST_BOARD.getTitle()).isEqualTo(TITLE),
+                () -> assertThat(TEST_BOARD.getContent()).isEqualTo(CONTENT),
+                () -> assertThat(TEST_BOARD.getWriter()).isEqualTo(WRITER),
+                () -> assertThat(TEST_BOARD.getCreatedDateTime()).isEqualTo(CREATED_DATE_TIME),
+                () -> assertThat(TEST_BOARD.isDeleted()).isFalse()
         );
     }
 }
