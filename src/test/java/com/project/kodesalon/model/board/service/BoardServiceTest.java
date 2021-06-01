98b0442cb8d90cc1cf05236bca11cc529d341554
Beenie93 <seongbeen93@naver.com>
2021-06-01 21:48:13 +0900

    test: #21 유효하지 않은 게시물 생성 요청 DTO -> entity로 변환 시 예외 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/board/service/BoardServiceTest.java
+++ src/test/java/com/project/kodesalon/model/board/service/BoardServiceTest.java
@@ -2,17 +2,24 @@ package com.project.kodesalon.model.board.service;
 
 import com.project.kodesalon.model.board.domain.Board;
 import com.project.kodesalon.model.board.domain.dto.BoardCreateRequestDto;
+import com.project.kodesalon.model.board.exception.ForbiddenException;
 import com.project.kodesalon.model.board.repository.BoardRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.stream.Stream;
+
 import static com.project.kodesalon.model.board.domain.BoardTest.CONTENT;
 import static com.project.kodesalon.model.board.domain.BoardTest.CREATED_DATE_TIME;
 import static com.project.kodesalon.model.board.domain.BoardTest.TITLE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -32,5 +39,23 @@ public class BoardServiceTest {
         BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(1L, TITLE, CONTENT, CREATED_DATE_TIME.toString());
         boardService.save(boardCreateRequestDto);
         verify(boardRepository, times(1)).save(any(Board.class));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForInvalidDto")
+    @DisplayName("Dto를 entity로 변환 시 유효하지 않은 값이 존재할 경우 예외가 발생한다.")
+    public void save_fail_invalid_dto(Long memberId, String title, String content, String createdDateTime) {
+        BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(memberId, title, content, createdDateTime);
+        assertThatThrownBy(() -> boardService.save(boardCreateRequestDto))
+                .isInstanceOf(ForbiddenException.class);
+    }
+
+    private static Stream<Arguments> provideArgumentsForInvalidDto() {
+        return Stream.of(
+                Arguments.of(1L, "", CONTENT, CREATED_DATE_TIME.toString()),
+                Arguments.of(1L, "this title length is 31        ", CONTENT, CREATED_DATE_TIME.toString()),
+                Arguments.of(1L, TITLE, "", CREATED_DATE_TIME.toString()),
+                Arguments.of(1L, TITLE, "1".repeat(501), CREATED_DATE_TIME.toString())
+        );
     }
 }
