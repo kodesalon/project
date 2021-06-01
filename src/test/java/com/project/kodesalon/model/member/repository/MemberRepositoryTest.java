473511f5074326f34cc2c8c875825b0400b37c30
sdrs6921 <sdrs6921@naver.com>
2021-06-01 19:30:30 +0900

    style : #20 불필요한 import 제거

--- src/test/java/com/project/kodesalon/model/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/project/kodesalon/model/member/repository/MemberRepositoryTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssertions.then;
