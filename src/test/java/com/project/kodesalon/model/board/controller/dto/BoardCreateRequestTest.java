0c3375f757c11e994560d810e23824e739fb134c
Beenie93 <seongbeen93@naver.com>
2021-06-03 04:55:45 +0900

    refactor: #21 인자 값 더 상세히 작성

--- src/test/java/com/project/kodesalon/model/board/controller/dto/BoardCreateRequestTest.java
+++ src/test/java/com/project/kodesalon/model/board/controller/dto/BoardCreateRequestTest.java
@@ -11,12 +11,12 @@ class BoardCreateRequestTest {
     @Test
     @DisplayName("게시물의 제목, 내용, 작성자, 생성 시간, 삭제 여부를 반환한다.")
     public void getter() {
-        BoardCreateRequest boardCreateRequest = new BoardCreateRequest(1L, "제목", "내용", "생성 시간");
+        BoardCreateRequest boardCreateRequest = new BoardCreateRequest(1L, "게시물 제목", "게시물 내용", "2021-06-01T23:59:59.999999");
         assertAll(
                 () -> assertThat(boardCreateRequest.getMemberId()).isEqualTo(1L),
-                () -> assertThat(boardCreateRequest.getTitle()).isEqualTo("제목"),
-                () -> assertThat(boardCreateRequest.getContent()).isEqualTo("내용"),
-                () -> assertThat(boardCreateRequest.getCreatedDateTime()).isEqualTo("생성 시간")
+                () -> assertThat(boardCreateRequest.getTitle()).isEqualTo("게시물 제목"),
+                () -> assertThat(boardCreateRequest.getContent()).isEqualTo("게시물 내용"),
+                () -> assertThat(boardCreateRequest.getCreatedDateTime()).isEqualTo("2021-06-01T23:59:59.999999")
         );
     }
 }
