68a967b21a75c588b6f01a9390f3581d397dc2c5
Beenie93 <seongbeen93@naver.com>
2021-06-03 03:02:51 +0900

    feat: #21 최상위 예외 클래스 BusinessException을 상속받는 유효하지 않은 값에 대한 예외 클래스 구현

---
+++ src/main/java/com/project/kodesalon/global/error/exception/InvalidValueException.java
@@ -0,0 +1,9 @@
+package com.project.kodesalon.global.error.exception;
+
+public class InvalidValueException extends BusinessException {
+
+    public InvalidValueException(String message) {
+        super(message);
+    }
+
+}
