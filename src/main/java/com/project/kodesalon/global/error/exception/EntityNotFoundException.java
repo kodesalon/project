01260d225e09d1108670d3525fe8a96b9256774b
Beenie93 <seongbeen93@naver.com>
2021-06-03 03:03:15 +0900

    feat: #21 최상위 예외 클래스 BusinessException을 상속받는 엔티티를 발견하지 못했을 경우 예외 클래스 구현

---
+++ src/main/java/com/project/kodesalon/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.project.kodesalon.global.error.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
