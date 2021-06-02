2db070eb07d4a08264e2bf8bf83fa1f3e547fdf7
Beenie93 <seongbeen93@naver.com>
2021-06-03 03:01:50 +0900

    feat: #21 최상위 예외 클래스 구현

---
+++ src/main/java/com/project/kodesalon/global/error/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.project.kodesalon.global.error.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
