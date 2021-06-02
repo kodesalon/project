4e91723a37c76763a6f373e1faf809f94704a5c5
Beenie93 <seongbeen93@naver.com>
2021-06-02 21:59:25 +0900

    refactor: #21 게시물 도메인이 DB에 저장되었는지 확인하는 테스트 변경

--- src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static com.project.kodesalon.model.board.domain.BoardTest.TEST_BOARD;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,7 +20,8 @@ public class BoardRepositoryTest {
     @Test
     @DisplayName("게시판 객체를 DB에 저장한다.")
     public void save() {
-        Board savedBoard = boardRepository.save(TEST_BOARD);
-        assertThat(TEST_BOARD.getId()).isEqualTo(savedBoard.getId());
+        boardRepository.save(TEST_BOARD);
+        Optional<Board> foundBoard = boardRepository.findById(TEST_BOARD.getId());
+        assertThat(foundBoard).isNotEmpty();
     }
 }
