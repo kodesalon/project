37f316cec316d835a02afa6fbed0a970abcca295
Beenie93 <seongbeen93@naver.com>
2021-06-03 05:15:57 +0900

    refactor: #21 repository save() 테스트 코드 수정

--- src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
@@ -1,14 +1,16 @@
 package com.project.kodesalon.model.board.repository;
 
 import com.project.kodesalon.model.board.domain.Board;
+import com.project.kodesalon.model.board.domain.vo.Content;
+import com.project.kodesalon.model.board.domain.vo.Title;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static com.project.kodesalon.model.board.domain.BoardTest.TEST_BOARD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -20,8 +22,16 @@ public class BoardRepositoryTest {
     @Test
     @DisplayName("게시판 객체를 DB에 저장한다.")
     public void save() {
-        boardRepository.save(TEST_BOARD);
-        Optional<Board> foundBoard = boardRepository.findById(TEST_BOARD.getId());
-        assertThat(foundBoard).isNotEmpty();
+        Board board = Board.builder()
+                .title(new Title("게시물 제목"))
+                .content(new Content("게시물 내용"))
+                .writer("작성자")
+                .createdDateTime(LocalDateTime.now())
+                .build();
+
+        boardRepository.save(board);
+        Optional<Board> possibleBoard = boardRepository.findById(board.getId());
+        assertThat(possibleBoard).isNotEmpty();
+        assertThat(possibleBoard.get().getId()).isEqualTo(1);
     }
 }
