53e1b74b11c664e006225b355617c4566736a536
Beenie93 <seongbeen93@naver.com>
2021-06-03 05:03:21 +0900

    refactor: #21 게시물 서비스에서 VO 유효성 관련 예외 발생 케이스 제거

--- src/main/java/com/project/kodesalon/model/board/service/BoardService.java
+++ src/main/java/com/project/kodesalon/model/board/service/BoardService.java
@@ -1,9 +1,6 @@
 package com.project.kodesalon.model.board.service;
 
 import com.project.kodesalon.model.board.domain.Board;
-import com.project.kodesalon.model.board.domain.vo.Content;
-import com.project.kodesalon.model.board.domain.vo.Title;
-import com.project.kodesalon.model.board.exception.InvalidArgumentException;
 import com.project.kodesalon.model.board.repository.BoardRepository;
 import com.project.kodesalon.model.board.service.dto.BoardCreateRequestDto;
 import org.springframework.stereotype.Service;
@@ -21,19 +18,11 @@ public class BoardService {
 
     @Transactional
     public void save(BoardCreateRequestDto boardCreateRequestDto) {
-        Board board = boardEntityFrom(boardCreateRequestDto);
-        boardRepository.save(board);
-    }
-
-    private Board boardEntityFrom(BoardCreateRequestDto boardCreateRequestDto) {
-        try {
-            return Board.builder()
-                    .title(new Title(boardCreateRequestDto.getTitle()))
-                    .content(new Content(boardCreateRequestDto.getContent()))
-                    .createdDateTime(LocalDateTime.parse(boardCreateRequestDto.getCreatedDateTime()))
-                    .build();
-        } catch (Exception e) {
-            throw new InvalidArgumentException(e.getMessage());
-        }
+        Board createdBoard = Board.builder()
+                .title(boardCreateRequestDto.getTitle())
+                .content(boardCreateRequestDto.getContent())
+                .createdDateTime(LocalDateTime.parse(boardCreateRequestDto.getCreatedDateTime()))
+                .build();
+        boardRepository.save(createdBoard);
     }
 }
--- src/test/java/com/project/kodesalon/model/board/service/BoardServiceTest.java
+++ src/test/java/com/project/kodesalon/model/board/service/BoardServiceTest.java
@@ -1,25 +1,17 @@
 package com.project.kodesalon.model.board.service;
 
 import com.project.kodesalon.model.board.domain.Board;
-import com.project.kodesalon.model.board.exception.InvalidArgumentException;
 import com.project.kodesalon.model.board.repository.BoardRepository;
 import com.project.kodesalon.model.board.service.dto.BoardCreateRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.stream.Stream;
+import java.time.LocalDateTime;
 
-import static com.project.kodesalon.model.board.domain.BoardTest.CONTENT;
-import static com.project.kodesalon.model.board.domain.BoardTest.CREATED_DATE_TIME;
-import static com.project.kodesalon.model.board.domain.BoardTest.TITLE;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -36,26 +28,8 @@ public class BoardServiceTest {
     @Test
     @DisplayName("컨트롤러에서 게시판 생성 요청 Dto를 전달받아 게시판을 생성한다.")
     public void save() {
-        BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(1L, TITLE, CONTENT, CREATED_DATE_TIME.toString());
+        BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(1L, "게시물 제목", "게시물 작성", LocalDateTime.now().toString());
         boardService.save(boardCreateRequestDto);
         verify(boardRepository, times(1)).save(any(Board.class));
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideArgumentsForInvalidDto")
-    @DisplayName("Dto를 entity로 변환 시 유효하지 않은 값이 존재할 경우 예외가 발생한다.")
-    public void save_fail_invalid_dto(Long memberId, String title, String content, String createdDateTime) {
-        BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(memberId, title, content, createdDateTime);
-        assertThatThrownBy(() -> boardService.save(boardCreateRequestDto))
-                .isInstanceOf(InvalidArgumentException.class);
-    }
-
-    private static Stream<Arguments> provideArgumentsForInvalidDto() {
-        return Stream.of(
-                Arguments.of(1L, "", CONTENT, CREATED_DATE_TIME.toString()),
-                Arguments.of(1L, "this title length is 31        ", CONTENT, CREATED_DATE_TIME.toString()),
-                Arguments.of(1L, TITLE, "", CREATED_DATE_TIME.toString()),
-                Arguments.of(1L, TITLE, "1".repeat(501), CREATED_DATE_TIME.toString())
-        );
     }
 }
