562deb2fea65d40da678b740873762f53fcbc2c5
Beenie93 <seongbeen93@naver.com>
2021-06-03 22:05:47 +0900

    test: ci/cd 테스트

---
+++ src/main/java/com/project/kodesalon/model/board/controller/dto/BoardCreateRequest2.java
@@ -0,0 +1,26 @@
+package com.project.kodesalon.model.board.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BoardCreateRequest2 {
+    private Long memberId;
+    private String title;
+    private String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdDateTime;
+
+    public BoardCreateRequest2(Long memberId, String title, String content, LocalDateTime createdDateTime) {
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+        this.createdDateTime = createdDateTime;
+    }
+}
+
