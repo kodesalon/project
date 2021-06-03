6b29593a98b1383b44a5b40f40d764b6f5ae8b94
Beenie93 <seongbeen93@naver.com>
2021-06-03 22:05:47 +0900

    test: ci/cd 테스트

--- src/main/java/com/project/kodesalon/model/board/controller/BoardController.java
+++ src/main/java/com/project/kodesalon/model/board/controller/BoardController.java
@@ -80,4 +80,10 @@ public class BoardController {
         System.out.println(boardCreateRequest.getCreatedDateTime());
         return new ResponseEntity(HttpStatus.CREATED);
     }
+
+    @PostMapping(value = "/boardss")
+    public ResponseEntity<HttpStatus> save2(@RequestBody final BoardCreateRequest boardCreateRequest) {
+        System.out.println(boardCreateRequest.getCreatedDateTime());
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 }
