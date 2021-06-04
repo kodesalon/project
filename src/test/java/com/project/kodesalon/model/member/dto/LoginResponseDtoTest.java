da0b28a24d56001c6a738f15d83c96226dd3212a
sdrs6921 <sdrs6921@naver.com>
2021-06-04 15:55:25 +0900

    refactor : #20 명확하지 않은 DisplayName 변경

--- src/test/java/com/project/kodesalon/model/member/dto/LoginResponseDtoTest.java
+++ src/test/java/com/project/kodesalon/model/member/dto/LoginResponseDtoTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LoginResponseDtoTest {
     @Test
-    @DisplayName("Loin이 성공하면 HttpStatus 200과 ID, ALIAS 객체를 생성합니다.")
+    @DisplayName("Loin이 성공하면 ID, ALIAS 객체를 생성합니다.")
     void login_success_create_success_login_response_dto() {
         LoginResponseDto loginResponseDto = new LoginResponseDto(1L, "alias");
         assertAll(
