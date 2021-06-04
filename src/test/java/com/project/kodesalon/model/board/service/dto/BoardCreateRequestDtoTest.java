893be263139ea76536af9eb8268d6acac42475cb
Beenie93 <seongbeen93@naver.com>
2021-06-04 16:07:36 +0900

    test: #21 Board 객체로 반환하는 함수 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/board/service/dto/BoardCreateRequestDtoTest.java
+++ src/test/java/com/project/kodesalon/model/board/service/dto/BoardCreateRequestDtoTest.java
@@ -1,5 +1,6 @@
 package com.project.kodesalon.model.board.service.dto;
 
+import com.project.kodesalon.model.board.domain.Board;
 import com.project.kodesalon.model.board.domain.vo.Content;
 import com.project.kodesalon.model.board.domain.vo.Title;
 import org.junit.jupiter.api.DisplayName;
@@ -11,16 +12,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class BoardCreateRequestDtoTest {
+    private final BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(1L, "게시물 제목", "게시물 내용", LocalDateTime.parse("2021-06-01T23:59:59.999999"));
 
     @Test
     @DisplayName("게시물의 제목, 내용, 작성자, 생성 시간, 삭제 여부를 반환한다.")
     public void getter() {
-        BoardCreateRequestDto boardCreateRequestDto = new BoardCreateRequestDto(1L, "게시물 제목", "게시물 내용", LocalDateTime.parse("2021-06-01T23:59:59.999999"));
         assertAll(
                 () -> assertThat(boardCreateRequestDto.getMemberId()).isEqualTo(1L),
                 () -> assertThat(boardCreateRequestDto.getTitle()).isEqualTo(new Title("게시물 제목")),
                 () -> assertThat(boardCreateRequestDto.getContent()).isEqualTo(new Content("게시물 내용")),
                 () -> assertThat(boardCreateRequestDto.getCreatedDateTime()).isEqualTo("2021-06-01T23:59:59.999999")
         );
+    }
+
+    @Test
+    @DisplayName("작성자를 입력받아, 멤버 객체를 반환한다.")
+    public void toBoard() {
+        Board board = boardCreateRequestDto.toBoard("writer");
+        assertThat(board.getWriter()).isEqualTo("writer");
     }
 }
