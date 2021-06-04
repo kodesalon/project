27112bce799ccbfc3ea560f9374ad67452bf8903
sdrs6921 <sdrs6921@naver.com>
2021-06-04 16:30:00 +0900

    refactor : #20 BddAssertions로 테스트 코드 통합

--- src/test/java/com/project/kodesalon/model/member/domain/vo/AliasTest.java
+++ src/test/java/com/project/kodesalon/model/member/domain/vo/AliasTest.java
@@ -5,9 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.then;
-
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
 class AliasTest {
     @Test
@@ -23,7 +22,7 @@ class AliasTest {
             "alias 1234", "1234", "a_______", "한글Alias"})
     @DisplayName("alias에 타당한 문자열 포맷이 아니면 예외를 리턴합니다.")
     void invalidateFormatAlias_throw_exception(String invalidFormat) {
-        assertThatThrownBy(() -> new Alias(invalidFormat)).isInstanceOf(IllegalArgumentException.class)
+        thenThrownBy(() -> new Alias(invalidFormat)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("아이디는 영문으로 시작해야 하며 4자리 이상 15자리 이하의 영문 혹은 숫자가 포함되어야 합니다.");
     }
 }
