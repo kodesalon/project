0aa11354fffb40a8dd96c18578f42afc06f543cd
sdrs6921 <sdrs6921@naver.com>
2021-06-04 23:52:49 +0900

    refactor : Service 레이어에 로그 추가

--- src/main/java/com/project/kodesalon/model/member/service/MemberService.java
+++ src/main/java/com/project/kodesalon/model/member/service/MemberService.java
@@ -5,14 +5,15 @@ import com.project.kodesalon.model.member.dto.CreateMemberRequestDto;
 import com.project.kodesalon.model.member.dto.LoginRequestDto;
 import com.project.kodesalon.model.member.dto.LoginResponseDto;
 import com.project.kodesalon.model.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
 @Service
+@Slf4j
 public class MemberService {
-    //TODO 예외 상황일 경우 구체적인 로그 남기기
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -21,27 +22,36 @@ public class MemberService {
 
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
         Member member = memberRepository.findMemberByAlias(loginRequestDto.getAlias())
-                .orElseThrow(() -> HttpClientErrorException.create("존재하는 아이디를 입력해주세요.", HttpStatus.UNAUTHORIZED,
-                        "", HttpHeaders.EMPTY, null, null));
+                .orElseThrow(() -> {
+                    log.error("로그인 단계에서 {}의 Alias가 존재하지 않음", loginRequestDto.getAlias());
+                    HttpClientErrorException.create("존재하는 아이디를 입력해주세요.", HttpStatus.UNAUTHORIZED,
+                            "", HttpHeaders.EMPTY, null, null);
+                    return null;
+                });
 
         if (member.isIncorrectPassword(loginRequestDto.getPassword())) {
+            log.error("{}의 Password가 일치하지 않음", loginRequestDto.getAlias());
             throw HttpClientErrorException.create("비밀 번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED,
                     "", HttpHeaders.EMPTY, null, null);
         }
 
+        log.info("ID : {} Alias : {} Member 로그안", member.getId(), member.getAlias());
         return new LoginResponseDto(member.getId(), member.getAlias());
     }
 
     public LoginResponseDto join(CreateMemberRequestDto createMemberRequestDto) {
         memberRepository.findMemberByAlias(createMemberRequestDto.getAlias())
                 .ifPresent(member -> {
+                    log.error("회원 가입 단계에서 {}는 이미 존재하는 Alias입니다.", createMemberRequestDto.getAlias());
                     throw new IllegalStateException("이미 존재하는 아이디입니다");
                 });
 
-        //TODO Member 밖으로 빼서 CreateMemberDto 내부 값을 모르게 하기
-        Member savedMember = memberRepository.save(new Member(createMemberRequestDto.getAlias().value(), createMemberRequestDto.getPassword().value(),
-                createMemberRequestDto.getName().value(), createMemberRequestDto.getEmail().value(), createMemberRequestDto.getPhone().value()));
+        Member saveMember = new Member(createMemberRequestDto.getAlias().value(), createMemberRequestDto.getPassword().value(),
+                createMemberRequestDto.getName().value(), createMemberRequestDto.getEmail().value(), createMemberRequestDto.getPhone().value());
 
-        return new LoginResponseDto(savedMember.getId(), savedMember.getAlias());
+        memberRepository.save(saveMember);
+
+        log.info("ID : {}, Alias : {} Member가 회원 가입 성공", saveMember.getId(), saveMember.getAlias());
+        return new LoginResponseDto(saveMember.getId(), saveMember.getAlias());
     }
 }
