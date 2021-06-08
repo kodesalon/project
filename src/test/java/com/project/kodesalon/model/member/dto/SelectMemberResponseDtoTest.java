8d22e0e7f236c4aee1e04dc9ba316b20440deb07
sdrs6921 <sdrs6921@naver.com>
2021-06-08 20:19:12 +0900

    refactor : #41 test 코드 bdd로 통합

--- src/test/java/com/project/kodesalon/model/member/dto/SelectMemberResponseDtoTest.java
+++ src/test/java/com/project/kodesalon/model/member/dto/SelectMemberResponseDtoTest.java
@@ -1,23 +1,22 @@
 package com.project.kodesalon.model.member.dto;
 
 
+import org.assertj.core.api.BDDSoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.BDDAssertions.then;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SelectMemberResponseDtoTest {
     @Test
     @DisplayName("생성자를 초기화 하면 필드가 초기화됩니다.")
     void create_constructor_init_filed() {
+        BDDSoftAssertions softly = new BDDSoftAssertions();
         SelectMemberResponseDto selectMemberResponseDto = new SelectMemberResponseDto("alias", "이름", "email@email.com", "010-1111-2222");
 
-        assertAll(
-                () -> then(selectMemberResponseDto.getAlias()).isEqualTo("alias"),
-                () -> then(selectMemberResponseDto.getName()).isEqualTo("이름"),
-                () -> then(selectMemberResponseDto.getEmail()).isEqualTo("email@email.com"),
-                () -> then(selectMemberResponseDto.getPhone()).isEqualTo("010-1111-2222")
-        );
+        softly.then(selectMemberResponseDto.getAlias()).isEqualTo("alias");
+        softly.then(selectMemberResponseDto.getName()).isEqualTo("이름");
+        softly.then(selectMemberResponseDto.getEmail()).isEqualTo("email@email.com");
+        softly.then(selectMemberResponseDto.getPhone()).isEqualTo("010-1111-2222");
+
+        softly.assertAll();
     }
 }
