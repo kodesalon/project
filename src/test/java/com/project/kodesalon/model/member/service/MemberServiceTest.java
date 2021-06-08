21eb056d18e878bbb50d39c94e80bdedecdac33b
sdrs6921 <sdrs6921@naver.com>
2021-06-08 20:58:24 +0900

    style : #41 테스트 코드 함수명 수정

--- src/test/java/com/project/kodesalon/model/member/service/MemberServiceTest.java
+++ src/test/java/com/project/kodesalon/model/member/service/MemberServiceTest.java
@@ -51,7 +51,7 @@ public class MemberServiceTest {
 
     @Test
     @DisplayName("존재하는 Alias가 Alias와 Password가 일치하면 Id, Alias를 리턴합니다.")
-    void exist_login_return_success2() {
+    void exist_login_return_success() {
         BDDSoftAssertions softly = new BDDSoftAssertions();
 
         given(member.getId()).willReturn(1L);
@@ -70,7 +70,7 @@ public class MemberServiceTest {
 
     @Test
     @DisplayName("존재하는 Alias가 Password가 일치하지 않는다면 예외 메세지를 발생시킵니다.")
-    void exist_login_return_fail2() {
+    void exist_login_return_fail() {
         given(member.isIncorrectPassword(loginRequestDto.getPassword())).willReturn(true);
         given(memberRepository.findMemberByAlias(loginRequestDto.getAlias()))
                 .willReturn(Optional.of(member));
