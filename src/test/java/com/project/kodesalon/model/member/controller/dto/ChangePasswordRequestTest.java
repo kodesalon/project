d4e1595ac4a645f5258c890d5a2925042a925ef6
Beenie93 <seongbeen93@naver.com>
2021-06-09 14:16:19 +0900

    test: #19 ChangePasswordRequest -> ChangePasswordRequestDto 반환하는 테스트 케이스 추가

--- src/test/java/com/project/kodesalon/model/member/controller/dto/ChangePasswordRequestTest.java
+++ src/test/java/com/project/kodesalon/model/member/controller/dto/ChangePasswordRequestTest.java
@@ -1,19 +1,28 @@
 package com.project.kodesalon.model.member.controller.dto;
 
+import com.project.kodesalon.model.member.service.dto.ChangePasswordRequestDto;
 import org.assertj.core.api.BDDSoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChangePasswordRequestTest {
+    private final BDDSoftAssertions softly = new BDDSoftAssertions();
+    private final ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest(1L, "ChangePassword1!");
 
     @Test
     @DisplayName("회원 식별 번호, 변경하려는 비밀번호를 반환한다.")
     public void getter() {
-        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest(1L, "ChangePassword1!");
-        BDDSoftAssertions softly = new BDDSoftAssertions();
-
         softly.then(changePasswordRequest.getMemberId()).isEqualTo(1L);
         softly.then(changePasswordRequest.getPassword()).isEqualTo("ChangePassword1!");
+        softly.assertAll();
+    }
+
+    @Test
+    @DisplayName("ChangePasswordRequestDto를 반환한다.")
+    public void toChangePasswordRequestDto() {
+        ChangePasswordRequestDto changePasswordRequestDto = changePasswordRequest.toChangePasswordRequestDto();
+        softly.then(changePasswordRequestDto.getMemberId()).isEqualTo(1L);
+        softly.then(changePasswordRequestDto.getPassword()).isEqualTo("ChangePassword1!");
         softly.assertAll();
     }
 }
