b74f75636d19be141c9bfdf810d8f7ffe27a00c7
Beenie93 <seongbeen93@naver.com>
2021-06-09 17:42:10 +0900

    refactor: #19 중복 메서드 제거

--- src/main/java/com/project/kodesalon/model/member/repository/MemberRepository.java
+++ src/main/java/com/project/kodesalon/model/member/repository/MemberRepository.java
@@ -4,17 +4,10 @@ package com.project.kodesalon.model.member.repository;
 import com.project.kodesalon.model.member.domain.Member;
 import com.project.kodesalon.model.member.domain.vo.Alias;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByAlias(Alias alias);
-
-    @Modifying
-    @Query("update Member m set m.password.password = :password where m.id = :memberId")
-    void updatePassword(@Param("password") String password, @Param("memberId") Long memberId);
 }
