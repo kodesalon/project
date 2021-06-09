403af7fcdbff0cfcfad2d8aaf5692c9a0ad2f8a0
Beenie93 <seongbeen93@naver.com>
2021-06-09 13:16:32 +0900

    test: #19 컨트롤러에서 매핑받는 DTO 테스트 케이스 추가

---
+++ src/test/java/com/project/kodesalon/model/member/controller/dto/ChangePasswordRequestTest.java
@@ -0,0 +1,19 @@
+package com.project.kodesalon.model.member.controller.dto;
+
+import org.assertj.core.api.BDDSoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChangePasswordRequestTest {
+
+    @Test
+    @DisplayName("회원 식별 번호, ")
+    public void getter() {
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest(1L, "ChangePassword1!");
+        BDDSoftAssertions softly = new BDDSoftAssertions();
+
+        softly.then(changePasswordRequest.getMemberId()).isEqualTo(1L);
+        softly.then(changePasswordRequest.getPassword()).isEqualTo("ChangePassword1!");
+        softly.assertAll();
+    }
+}
