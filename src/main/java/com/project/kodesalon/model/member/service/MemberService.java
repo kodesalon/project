e7dc4af621749f4e362704a48c0ef49f5de74f28
Beenie93 <seongbeen93@naver.com>
2021-06-14 05:12:20 +0900

    refactor: #18 회원 Service의 LoginRequestDto를 LoginRequest로 교체

--- src/main/java/com/project/kodesalon/model/member/service/MemberService.java
+++ src/main/java/com/project/kodesalon/model/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.project.kodesalon.model.member.service;
 
+import com.project.kodesalon.model.member.controller.dto.LoginRequest;
 import com.project.kodesalon.model.member.domain.Member;
+import com.project.kodesalon.model.member.domain.vo.Alias;
+import com.project.kodesalon.model.member.domain.vo.Password;
 import com.project.kodesalon.model.member.repository.MemberRepository;
 import com.project.kodesalon.model.member.service.dto.CreateMemberRequestDto;
-import com.project.kodesalon.model.member.service.dto.LoginRequestDto;
 import com.project.kodesalon.model.member.service.dto.LoginResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,19 +21,21 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
-    public LoginResponseDto login(LoginRequestDto loginRequestDto) throws HttpClientErrorException {
-        Member member = memberRepository.findMemberByAlias(loginRequestDto.getAlias())
+    public LoginResponseDto login(LoginRequest loginRequest) throws HttpClientErrorException {
+        String alias = loginRequest.getAlias();
+        Member member = memberRepository.findMemberByAlias(new Alias(alias))
                 .orElseThrow(() -> {
-                    log.info("{}인 Alias를 가진 사용자가 존재하지 않음", loginRequestDto.getAlias().value());
+                    log.info("{}인 Alias를 가진 사용자가 존재하지 않음", alias);
                     throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "존재하는 아이디를 입력해주세요.");
                 });
 
-        if (!member.hasSamePassword(loginRequestDto.getPassword())) {
-            log.info("{}의 Password가 일치하지 않음", loginRequestDto.getAlias().value());
+        String password = loginRequest.getPassword();
+        if (!member.hasSamePassword(new Password(password))) {
+            log.info("{}의 Password가 일치하지 않음", alias);
             throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "비밀 번호가 일치하지 않습니다.");
         }
 
-        log.info("ID : {} Alias : {} Member 로그인", member.getId(), member.getAlias());
+        log.info("ID : {}, Alias : {} Member 로그인", member.getId(), member.getAlias());
         return new LoginResponseDto(member.getId(), member.getAlias());
     }
 
--- src/test/java/com/project/kodesalon/model/member/service/MemberServiceTest.java
+++ src/test/java/com/project/kodesalon/model/member/service/MemberServiceTest.java
@@ -1,10 +1,11 @@
 package com.project.kodesalon.model.member.service;
 
+import com.project.kodesalon.model.member.controller.dto.LoginRequest;
 import com.project.kodesalon.model.member.domain.Member;
 import com.project.kodesalon.model.member.domain.vo.Alias;
+import com.project.kodesalon.model.member.domain.vo.Password;
 import com.project.kodesalon.model.member.repository.MemberRepository;
 import com.project.kodesalon.model.member.service.dto.CreateMemberRequestDto;
-import com.project.kodesalon.model.member.service.dto.LoginRequestDto;
 import com.project.kodesalon.model.member.service.dto.LoginResponseDto;
 import org.assertj.core.api.BDDSoftAssertions;
 import org.junit.jupiter.api.DisplayName;
@@ -24,7 +25,7 @@ import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
     private final BDDSoftAssertions softly = new BDDSoftAssertions();
-    private final LoginRequestDto loginRequestDto = new LoginRequestDto("alias", "Password123!!");
+    private final LoginRequest loginRequest = new LoginRequest("alias", "Password123!!");
     private final CreateMemberRequestDto createMemberRequestDto = new CreateMemberRequestDto("alias", "Password123!!", "이름", "email@email.com", "010-1111-2222");
 
     @InjectMocks
@@ -39,13 +40,12 @@ public class MemberServiceTest {
     @Test
     @DisplayName("로그인 성공하면 회원 식별자, 별명을 담은 DTO를 반환합니다.")
     void login() {
-        LoginRequestDto loginRequestDto = new LoginRequestDto("alias", "Password123!!");
         given(member.getId()).willReturn(1L);
         given(member.getAlias()).willReturn("alias");
-        given(member.hasSamePassword(loginRequestDto.getPassword())).willReturn(true);
-        given(memberRepository.findMemberByAlias(loginRequestDto.getAlias())).willReturn(Optional.of(member));
+        given(member.hasSamePassword(new Password(loginRequest.getPassword()))).willReturn(true);
+        given(memberRepository.findMemberByAlias(new Alias(loginRequest.getAlias()))).willReturn(Optional.of(member));
 
-        LoginResponseDto loginResponseDto = memberService.login(loginRequestDto);
+        LoginResponseDto loginResponseDto = memberService.login(loginRequest);
 
         softly.then(loginResponseDto.getMemberId()).isEqualTo(1L);
         softly.then(loginResponseDto.getAlias()).isEqualTo("alias");
@@ -55,9 +55,9 @@ public class MemberServiceTest {
     @Test
     @DisplayName("로그인 시 존재하지 않는 아이디(Alias)일 경우, 예외가 발생합니다.")
     void login_throw_exception_with_invalid_alias() {
-        given(memberRepository.findMemberByAlias(loginRequestDto.getAlias())).willReturn(Optional.empty());
+        given(memberRepository.findMemberByAlias(new Alias(loginRequest.getAlias()))).willReturn(Optional.empty());
 
-        thenThrownBy(() -> memberService.login(loginRequestDto))
+        thenThrownBy(() -> memberService.login(loginRequest))
                 .isInstanceOf(HttpClientErrorException.class)
                 .hasMessageContaining("존재하는 아이디를 입력해주세요.");
     }
@@ -65,12 +65,10 @@ public class MemberServiceTest {
     @Test
     @DisplayName("로그인 시 비밀번호 틀렸을 경우, 예외 메세지를 반환합니다.")
     void login_throw_exception_with_invalid_password() {
-        LoginRequestDto loginRequestDto = new LoginRequestDto("alias", "Password123!!!");
+        given(member.hasSamePassword(new Password(loginRequest.getPassword()))).willReturn(false);
+        given(memberRepository.findMemberByAlias(new Alias(loginRequest.getAlias()))).willReturn(Optional.of(member));
 
-        given(member.hasSamePassword(loginRequestDto.getPassword())).willReturn(false);
-        given(memberRepository.findMemberByAlias(loginRequestDto.getAlias())).willReturn(Optional.of(member));
-
-        thenThrownBy(() -> memberService.login(loginRequestDto))
+        thenThrownBy(() -> memberService.login(loginRequest))
                 .isInstanceOf(HttpClientErrorException.class)
                 .hasMessageContaining("비밀 번호가 일치하지 않습니다.");
     }
