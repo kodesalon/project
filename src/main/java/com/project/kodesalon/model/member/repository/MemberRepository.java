beb3fc491a65a5d5bfa46faeb9468b5fee7c182e
sdrs6921 <sdrs6921@naver.com>
2021-06-16 18:39:03 +0900

    refactor : #45 Repository deleteById 중복 정의 삭제

--- src/main/java/com/project/kodesalon/model/member/repository/MemberRepository.java
+++ src/main/java/com/project/kodesalon/model/member/repository/MemberRepository.java
@@ -10,6 +10,4 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByAlias(Alias alias);
-
-    void deleteById(Long memberId);
 }
