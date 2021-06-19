3dcddfc7f1c8a7b219d2400355b8adf4ccf017c7
Beenie93 <seongbeen93@naver.com>
2021-06-20 03:22:09 +0900

    refactor: #51 BoardService의 MemberRepository 의존성 제거

--- src/main/java/com/project/kodesalon/model/board/service/BoardService.java
+++ src/main/java/com/project/kodesalon/model/board/service/BoardService.java
@@ -4,33 +4,25 @@ import com.project.kodesalon.model.board.domain.Board;
 import com.project.kodesalon.model.board.repository.BoardRepository;
 import com.project.kodesalon.model.board.service.dto.BoardCreateRequest;
 import com.project.kodesalon.model.member.domain.Member;
-import com.project.kodesalon.model.member.repository.MemberRepository;
+import com.project.kodesalon.model.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
 public class BoardService {
     private final BoardRepository boardRepository;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public BoardService(BoardRepository boardRepository, MemberRepository memberRepository) {
+    public BoardService(final BoardRepository boardRepository, final MemberService memberService) {
         this.boardRepository = boardRepository;
-        this.memberRepository = memberRepository;
+        this.memberService = memberService;
     }
 
     @Transactional
-    public void save(BoardCreateRequest boardCreateRequest) {
-        Long memberId = boardCreateRequest.getMemberId();
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> {
-                    log.info("게시물 저장 단계에서 존재하지 않는 회원 식별자 memberId : {}", memberId);
-                    throw new NoSuchElementException("찾으려는 회원이 없습니다");
-                });
-
+    public void save(final BoardCreateRequest boardCreateRequest) {
+        Member member = memberService.findById(boardCreateRequest.getMemberId());
         Board createdBoard = boardCreateRequest.toBoard(member);
         boardRepository.save(createdBoard);
     }
