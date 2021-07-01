134e1f14480c96166046b903ada76b60a4508d6d
Beenie93 <seongbeen93@naver.com>
2021-07-02 03:38:04 +0900

    feat: #57 LoginInterceptor 수정 및 @Login ArgumentResolver 구현

---
+++ src/main/java/com/project/kodesalon/common/ErrorCode.java
@@ -0,0 +1,8 @@
+package com.project.kodesalon.common;
+
+public class ErrorCode {
+
+    // JWT
+    public static final String EXPIRED_JWT_TOKEN = "J001";
+    public static final String INVALID_JWT_TOKEN = "J002";
+}
