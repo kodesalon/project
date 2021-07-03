efeb38c7fbdcc1c78114d5602f73e7c61c577515
sdrs6921 <sdrs6921@naver.com>
2021-07-04 01:04:12 +0900

    refactor : #57 serverUrl value 추가

--- src/main/java/com/project/kodesalon/common/config/WebConfig.java
+++ src/main/java/com/project/kodesalon/common/config/WebConfig.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginInterceptor loginInterceptor;
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Value("${spring.server.url}")
+    private String serverUrl;
 
     public WebConfig(LoginInterceptor loginInterceptor, LoginMemberArgumentResolver loginMemberArgumentResolver) {
         this.loginInterceptor = loginInterceptor;
