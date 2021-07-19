4b35eb92be65fdee2b342136ff961af3ca4603ba
sdrs6921 <sdrs6921@naver.com>
2021-07-19 16:40:33 +0900

    refactor : #67 Cross Origin 적용

--- src/main/java/com/project/kodesalon/common/config/WebConfig.java
+++ src/main/java/com/project/kodesalon/common/config/WebConfig.java
@@ -23,7 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 
     @Override
