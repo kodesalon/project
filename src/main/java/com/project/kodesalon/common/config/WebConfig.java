344db8b084b91fe4b4e1aa85b25f6398ff8cde97
Beenie93 <seongbeen93@naver.com>
2021-07-19 20:59:13 +0900

    refactor : allowedMethods 적용

--- src/main/java/com/project/kodesalon/common/config/WebConfig.java
+++ src/main/java/com/project/kodesalon/common/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedMethods("*");
     }
 
     @Override
