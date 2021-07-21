b47d255274582701c33ffe351f364c9cf563e74c
Beenie93 <seongbeen93@naver.com>
2021-07-21 13:11:31 +0900

    refactor : #24 @columnDefinition 적용

--- src/main/java/com/project/kodesalon/model/board/domain/Board.java
+++ src/main/java/com/project/kodesalon/model/board/domain/Board.java
@@ -43,8 +43,8 @@ public class Board extends BaseEntity {
     @JoinColumn(name = "member_id", nullable = false)
     private Member writer;
 
-    @Column(name = "deleted")
-    private boolean deleted = false;
+    @Column(name = "deleted", columnDefinition = "boolean default false")
+    private boolean deleted;
 
     public Board(final String title, final String content, final Member writer, final LocalDateTime createdDateTime) {
         this.title = new Title(title);
--- src/main/java/com/project/kodesalon/model/member/domain/Member.java
+++ src/main/java/com/project/kodesalon/model/member/domain/Member.java
@@ -62,8 +62,8 @@ public class Member extends BaseEntity {
     @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
     private List<Board> boards = new ArrayList<>();
 
-    @Column(name = "deleted")
-    private boolean deleted = false;
+    @Column(name = "deleted", columnDefinition = "boolean default false")
+    private boolean deleted;
 
     public Member(final String alias, final String password, final String name, final String email, final String phone, LocalDateTime createdDateTime) {
         this.alias = new Alias(alias);
