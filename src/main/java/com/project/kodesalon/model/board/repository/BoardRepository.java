3be47c4461b43e813f9c999730992b0b06c7fc10
Beenie93 <seongbeen93@naver.com>
2021-07-21 13:32:54 +0900

    refactor : #24 @Repository 어노테이션 제거

--- src/main/java/com/project/kodesalon/model/board/repository/BoardRepository.java
+++ src/main/java/com/project/kodesalon/model/board/repository/BoardRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Modifying(flushAutomatically = true)
