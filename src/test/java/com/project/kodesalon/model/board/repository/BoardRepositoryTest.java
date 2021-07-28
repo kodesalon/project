2111cd39b4439704bc952fc576472bd17601499b
sdrs6921 <sdrs6921@naver.com>
2021-07-28 12:04:17 +0900

    refactor : #22 BoardRepository 테스트 독립적으로 수행 되도록 수정

--- src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
@@ -3,13 +3,14 @@ package com.project.kodesalon.model.board.repository;
 import com.project.kodesalon.model.board.domain.Board;
 import com.project.kodesalon.model.member.domain.Member;
 import org.assertj.core.api.BDDSoftAssertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnitUtil;
 import java.time.LocalDateTime;
@@ -25,7 +26,7 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Autowired
-    private TestEntityManager testEntityManager;
+    private EntityManager entityManager;
 
     @Autowired
     private EntityManagerFactory entityManagerFactory;
@@ -39,31 +40,42 @@ public class BoardRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        member = new Member("alias", "Password!!123", "이름", "email@email.com", "010-1234-4444", LocalDateTime.now());
-        testEntityManager.persist(member);
-        board = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
-        board = boardRepository.save(board);
-        testEntityManager.flush();
-        testEntityManager.clear();
         persistenceUnitUtil = entityManagerFactory.getPersistenceUnitUtil();
+    }
+
+    @AfterEach
+    void tearDown() {
+        boardRepository.deleteAll();
+        entityManager.createNativeQuery("ALTER TABLE board ALTER COLUMN `board_id` RESTART WITH 1")
+                .executeUpdate();
     }
 
     @Test
     @DisplayName("게시판 객체를 DB에 저장한다.")
     void save() {
+        Member member = new Member("alias", "Password!!123", "이름", "email@email.com", "010-1234-4444", LocalDateTime.now());
+        entityManager.persist(member);
+        Board board = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
+
+        board = boardRepository.save(board);
+
         then(board.getId()).isNotNull();
     }
+
 
     @Test
     @DisplayName("회원 식별 번호를 입력받으면 해당 작성 식별 번호를 가진 게시물의 deleted를 true로 변환한다.")
     void deleteBoardByMemberId() {
+        Member member = new Member("alias", "Password!!123", "이름", "email@email.com", "010-1234-4444", LocalDateTime.now());
+        entityManager.persist(member);
         Board firstBoard = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
         Board secondBoard = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
         boardRepository.save(firstBoard);
         boardRepository.save(secondBoard);
 
         boardRepository.deleteBoardByMemberId(member.getId());
-        testEntityManager.clear();
+        entityManager.flush();
+        entityManager.clear();
 
         softly.then(boardRepository.findById(firstBoard.getId())).isEmpty();
         softly.then(boardRepository.findById(secondBoard.getId())).isEmpty();
@@ -73,23 +85,32 @@ public class BoardRepositoryTest {
     @Test
     @DisplayName("게시물 식별 번호를 입력받아 게시물을 조회하면 작성자 정보와 함께 조인하여 반환한다.")
     void selectBoard() {
-        Optional<Board> board = boardRepository.selectBoardById(this.board.getId());
+        Member member = new Member("alias", "Password!!123", "이름", "email@email.com", "010-1234-4444", LocalDateTime.now());
+        entityManager.persist(member);
+        Board board = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
+        boardRepository.save(board);
+        entityManager.flush();
+        entityManager.clear();
 
-        softly.then(board).isNotEmpty();
-        softly.then(persistenceUnitUtil.isLoaded(board.get().getWriter())).isTrue();
+        Optional<Board> selectedBoard = boardRepository.selectBoardById(board.getId());
+
+        softly.then(selectedBoard).isNotEmpty();
+        softly.then(persistenceUnitUtil.isLoaded(selectedBoard.get().getWriter())).isTrue();
     }
 
     @Test
     @DisplayName("마지막으로 조회한 게시물 번호, 조회할 게시물 수를 입력받아 게시물과 작성자의 정보를 조인하여 반환한다.")
     void selectBoards() {
+        Member member = new Member("alias", "Password!!123", "이름", "email@email.com", "010-1234-4444", LocalDateTime.now());
+        entityManager.persist(member);
         int boardToBeSelectedAtOnce = 10;
 
         for (int board_number = 0; board_number <= boardToBeSelectedAtOnce; board_number++) {
             Board board = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
             boardRepository.save(board);
         }
-        testEntityManager.flush();
-        testEntityManager.clear();
+        entityManager.flush();
+        entityManager.clear();
 
         List<Board> boards = boardRepository.selectBoards(10000L, boardToBeSelectedAtOnce);
 
