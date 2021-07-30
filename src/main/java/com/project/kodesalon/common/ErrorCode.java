b159c7f9d2bd2768e0b9c137045b55dfcf440918
Beenie93 <seongbeen93@naver.com>
2021-07-31 00:31:23 +0900

    refactor: 사용하지 않는 `INVALID_BOARD_ID` 에러 code 제거

--- src/main/java/com/project/kodesalon/common/ErrorCode.java
+++ src/main/java/com/project/kodesalon/common/ErrorCode.java
@@ -19,8 +19,7 @@ public class ErrorCode {
     public static final String INVALID_BOARD_CONTENT = "B002";
     public static final String NOT_EXIST_BOARD = "B003";
     public static final String NOT_AUTHORIZED_MEMBER = "B004";
-    public static final String INVALID_BOARD_ID = "B005";
-    public static final String ALREADY_DELETED_BOARD = "B006";
+    public static final String ALREADY_DELETED_BOARD = "B005";
 
     // JWT
     public static final String EXPIRED_JWT_TOKEN = "J001";
