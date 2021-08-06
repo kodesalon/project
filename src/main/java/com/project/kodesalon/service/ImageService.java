9f19467b03b97350928e409c974a96e2ac1fb452
Beenie93 <seongbeen93@naver.com>
2021-08-06 13:28:34 +0900

    feat: #64 ImageService 이미지 저장 기능 구현

---
+++ src/main/java/com/project/kodesalon/service/ImageService.java
@@ -0,0 +1,37 @@
+package com.project.kodesalon.service;
+
+import com.project.kodesalon.domain.Image;
+import com.project.kodesalon.domain.board.Board;
+import com.project.kodesalon.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ImageService {
+    private final ImageRepository imageRepository;
+    private final S3Uploader s3Uploader;
+    private final String directory;
+
+    public ImageService(final ImageRepository imageRepository, final S3Uploader s3Uploader,
+                        @Value("${cloud.aws.s3.image.directory}") final String directory) {
+        this.imageRepository = imageRepository;
+        this.s3Uploader = s3Uploader;
+        this.directory = directory;
+    }
+
+    public void save(final List<MultipartFile> multipartFiles, final Board board) throws IOException {
+        if (multipartFiles == null) {
+            return;
+        }
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            String url = s3Uploader.upload(multipartFile, directory);
+            Image image = new Image(url, board);
+            imageRepository.save(image);
+        }
+    }
+}
---
+++ src/test/java/com/project/kodesalon/service/ImageServiceTest.java
@@ -0,0 +1,58 @@
+package com.project.kodesalon.service;
+
+import com.project.kodesalon.domain.Image;
+import com.project.kodesalon.domain.board.Board;
+import com.project.kodesalon.repository.ImageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ImageServiceTest {
+
+    private ImageService imageService;
+
+    @Mock
+    private ImageRepository imageRepository;
+
+    @Mock
+    private S3Uploader s3Uploader;
+
+    @Mock
+    private Board board;
+
+    @Mock
+    private MultipartFile multipartFile;
+
+    @BeforeEach
+    void setUp() {
+        imageService = new ImageService(imageRepository, s3Uploader, "directory");
+    }
+
+    @Test
+    @DisplayName("파일을 전달받아 이미지를 저장한다.")
+    void save() throws IOException {
+        given(s3Uploader.upload(any(MultipartFile.class), anyString())).willReturn("localhost:8080/bucket/directory/image.jpeg");
+        List<MultipartFile> multipartFiles = Arrays.asList(multipartFile, multipartFile);
+        int imageSize = multipartFiles.size();
+
+        imageService.save(multipartFiles, board);
+
+        verify(s3Uploader, times(imageSize)).upload(any(MultipartFile.class), anyString());
+        verify(imageRepository, times(imageSize)).save(any(Image.class));
+    }
+}
