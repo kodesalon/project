691525db82d84340fee1262c13974176eeb06d3a
sdrs6921 <sdrs6921@naver.com>
2021-08-09 15:31:57 +0900

    refactor : #64 S3MockConfiguration 파일 설정 추가

--- src/test/java/com/project/kodesalon/service/S3UploaderTest.java
+++ src/test/java/com/project/kodesalon/service/S3UploaderTest.java
@@ -1,17 +1,15 @@
 package com.project.kodesalon.service;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.AnonymousAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.project.kodesalon.config.S3MockConfiguration;
 import io.findify.s3mock.S3Mock;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -21,53 +19,38 @@ import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
 @ExtendWith(SpringExtension.class)
-public class S3UploaderTest {
+@Import(S3MockConfiguration.class)
+class S3UploaderTest {
 
     public static final String BUCKET = "testbucket";
 
-    private S3Uploader s3Uploader;
+    @Autowired
     private S3Mock s3Mock;
-    private AmazonS3 client;
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    private S3Uploader s3Uploader;
 
     @BeforeEach
     void setUp() {
-        s3Mock = new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
-        s3Mock.start();
-
-        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration("http://localhost:8001", "ap-northeast-2");
-        client = AmazonS3ClientBuilder
-                .standard()
-                .withPathStyleAccessEnabled(true)
-                .withEndpointConfiguration(endpoint)
-                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
-                .build();
-
-        client.createBucket(BUCKET);
-        s3Uploader = new S3Uploader(client, BUCKET);
-    }
-
-    @AfterEach
-    void tearDown() {
-        s3Mock.shutdown();
+        s3Uploader = new S3Uploader(amazonS3, "testbucket");
     }
 
     @Test
     @DisplayName("S3 bucket에 이미지 파일을 저장하고, 저장한 경로를 반환한다.")
     void upload() throws IOException {
-        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "mock1.png",
-                "image/png", "test data".getBytes());
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "mock1.png", "image/png", "test data".getBytes());
 
         String result = s3Uploader.upload(mockMultipartFile, "static");
 
-        System.out.println("TEST: " + result);
         then(result).isNotNull();
     }
 
     @Test
     @DisplayName("S3 bucket에 있는 이미지 파일을 삭제한다.")
     void delete() throws IOException {
-        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "mock1.png",
-                "image/png", "test data".getBytes());
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "mock1.png", "image/png", "test data".getBytes());
         String fileUrl = s3Uploader.upload(mockMultipartFile, "static");
         int indexOfFileName = fileUrl.lastIndexOf("/");
         String fileUrlWithoutFileName = fileUrl.substring(0, indexOfFileName);
@@ -76,7 +59,7 @@ public class S3UploaderTest {
 
         s3Uploader.delete(key);
 
-        thenThrownBy(() -> client.getObject(BUCKET, key))
+        thenThrownBy(() -> amazonS3.getObject(BUCKET, key))
                 .isInstanceOf(AmazonS3Exception.class);
     }
 }
