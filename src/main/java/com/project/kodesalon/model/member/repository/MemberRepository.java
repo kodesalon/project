9136604a578a61f69426c0bb15fbc1aeef1742c0
sdrs6921 <sdrs6921@naver.com>
2021-08-10 15:45:37 +0900

    refactor : #64 회원 조회시, 자신이 올린 게시물 join 메서드 추가

--- src/main/java/com/project/kodesalon/model/member/repository/MemberRepository.java
+++ src/main/java/com/project/kodesalon/model/member/repository/MemberRepository.java
@@ -4,10 +4,15 @@ package com.project.kodesalon.model.member.repository;
 import com.project.kodesalon.model.member.domain.Member;
 import com.project.kodesalon.model.member.domain.vo.Alias;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findMemberByAlias(final Alias alias);
+
+    @Query("select m from Member m join fetch m.boards where m.id = :memberId")
+    Optional<Member> selectMemberById(@Param("memberId") final Long memberId);
 }
--- src/test/java/com/project/kodesalon/model/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/project/kodesalon/model/member/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.project.kodesalon.model.member.repository;
 
+import com.project.kodesalon.model.board.domain.Board;
 import com.project.kodesalon.model.member.domain.Member;
 import com.project.kodesalon.model.member.domain.vo.Alias;
 import org.assertj.core.api.BDDSoftAssertions;
@@ -9,7 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceUnitUtil;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static com.project.kodesalon.common.ErrorCode.NOT_EXIST_MEMBER_ALIAS;
@@ -19,18 +25,27 @@ import static org.assertj.core.api.BDDAssertions.then;
 @DataJpaTest
 public class MemberRepositoryTest {
 
+    private final BDDSoftAssertions softly = new BDDSoftAssertions();
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    private PersistenceUnitUtil persistenceUnitUtil;
 
     @BeforeEach
     void setUp() {
         memberRepository.save(TEST_MEMBER);
+        persistenceUnitUtil = entityManagerFactory.getPersistenceUnitUtil();
     }
 
     @Test
     @DisplayName("DB에 존재하는 회원을 아이디(Alias)로 조회할 경우, 해당 아이디를 가진 회원을 리턴합니다")
     void findMemberByAlias() {
-        BDDSoftAssertions softly = new BDDSoftAssertions();
         Member savedMember = memberRepository.findMemberByAlias(new Alias("alias"))
                 .orElseThrow(() -> new EntityNotFoundException(NOT_EXIST_MEMBER_ALIAS));
 
@@ -49,5 +64,27 @@ public class MemberRepositoryTest {
         Optional<Member> notPresentMember = memberRepository.findMemberByAlias(new Alias("notAlias"));
 
         then(notPresentMember).isEmpty();
+    }
+
+    @Test
+    @DisplayName("회원 식별 번호를 입력받아 회원 정보와 회원이 올린 게시물 정보와 조인하여 반환한다")
+    void findMemberById() {
+        int size = 5;
+        Member member
+                = new Member("alias1", "Password123!", "이름", "email@email.com", "010-1111-2222", LocalDateTime.now());
+        memberRepository.save(member);
+        for (int i = 0; i < size; i++) {
+            Board myBoard = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
+            entityManager.persist(myBoard);
+        }
+        entityManager.flush();
+        entityManager.clear();
+
+        Member selectMember = memberRepository.selectMemberById(member.getId()).get();
+
+        List<Board> boards = selectMember.getBoards();
+
+        boards.forEach(board -> softly.then(persistenceUnitUtil.isLoaded(board)).isTrue());
+        softly.assertAll();
     }
 }
