1afb8721ca2f9989b5ca0edb03684884efc52674
sdrs6921 <sdrs6921@naver.com>
2021-08-26 20:14:36 +0900

    refactor : #72 DBUnitTestConfiguration 생성

---
+++ src/test/java/com/project/kodesalon/config/DBUnitTestConfiguration.java
@@ -0,0 +1,30 @@
+package com.project.kodesalon.config;
+
+import com.github.springtestdbunit.bean.DatabaseConfigBean;
+import com.github.springtestdbunit.bean.DatabaseDataSourceConnectionFactoryBean;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+
+@TestConfiguration
+public class DBUnitTestConfiguration {
+
+    @Bean
+    public DatabaseConfigBean dbUnitDatabaseConfig() {
+        DatabaseConfigBean databaseConfigBean = new DatabaseConfigBean();
+        databaseConfigBean.setAllowEmptyFields(true);
+        databaseConfigBean.setDatatypeFactory(new H2DataTypeFactory());
+        return databaseConfigBean;
+    }
+
+    @Bean
+    public DatabaseDataSourceConnectionFactoryBean dbUnitDatabaseConnection(final DataSource dataSource) {
+        DatabaseDataSourceConnectionFactoryBean dbUnitDatabaseConnection = new DatabaseDataSourceConnectionFactoryBean();
+        dbUnitDatabaseConnection.setDataSource(dataSource);
+        dbUnitDatabaseConnection.setDatabaseConfig(dbUnitDatabaseConfig());
+        return dbUnitDatabaseConnection;
+    }
+}
