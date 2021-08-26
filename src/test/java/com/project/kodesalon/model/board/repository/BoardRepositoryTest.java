916149d3c4c03376599bd665694c7b2a76324445
sdrs6921 <sdrs6921@naver.com>
2021-08-26 20:15:16 +0900

    refactor : #72 RepositoryTest 데이터 셋 사용하도록 변경

--- src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/project/kodesalon/model/board/repository/BoardRepositoryTest.java
@@ -1,58 +1,40 @@
 package com.project.kodesalon.model.board.repository;
 
-import com.project.kodesalon.model.board.domain.Board;
-import com.project.kodesalon.model.member.domain.Member;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.project.kodesalon.config.DBUnitTestConfiguration;
 import org.assertj.core.api.BDDSoftAssertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
-import java.time.LocalDateTime;
-
-import static org.assertj.core.api.BDDAssertions.then;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 @DataJpaTest
+@Import(DBUnitTestConfiguration.class)
+@DbUnitConfiguration(databaseConnection = "dbUnitDatabaseConnection")
+@DatabaseSetup(value = "classpath:board.xml", type = DatabaseOperation.CLEAN_INSERT)
+@DatabaseTearDown(value = "classpath:board.xml", type = DatabaseOperation.DELETE_ALL)
+@TestExecutionListeners({DbUnitTestExecutionListener.class, DependencyInjectionTestExecutionListener.class})
 public class BoardRepositoryTest {
 
     @Autowired
     private BoardRepository boardRepository;
 
-    @Autowired
-    private TestEntityManager testEntityManager;
-
-    private Member member;
-
-    @BeforeEach
-    void setUp() {
-        member = new Member("alias", "Password!!123", "이름", "email@email.com", "010-1234-4444", LocalDateTime.now());
-        testEntityManager.persist(member);
-    }
-
-    @Test
-    @DisplayName("게시판 객체를 DB에 저장한다.")
-    void save() {
-        Board board = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
-        board = boardRepository.save(board);
-        then(board.getId()).isNotNull();
-    }
-
     @Test
     @DisplayName("회원 식별 번호를 입력받으면 해당 작성 식별 번호를 가진 게시물의 deleted를 true로 변환한다.")
     void deleteBoardByMemberId() {
         BDDSoftAssertions softly = new BDDSoftAssertions();
-        Board firstBoard = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
-        Board secondBoard = new Board("게시물 제목", "게시물 내용", member, LocalDateTime.now());
-        boardRepository.save(firstBoard);
-        boardRepository.save(secondBoard);
+        boardRepository.deleteBoardByMemberId(1L);
 
-        boardRepository.deleteBoardByMemberId(member.getId());
-        testEntityManager.clear();
-
-        softly.then(boardRepository.findById(firstBoard.getId())).isEmpty();
-        softly.then(boardRepository.findById(secondBoard.getId())).isEmpty();
+        softly.then(boardRepository.findById(1L)).isEmpty();
+        softly.then(boardRepository.findById(3L)).isEmpty();
         softly.assertAll();
     }
 }
+
