07ffefd124219606a846d31852cfae5003c5a56e
Beenie93 <seongbeen93@naver.com>
2021-09-08 15:36:21 +0900

    refactor: #22 게시물 조회 DTO 제네릭 적용

--- src/main/java/com/project/kodesalon/model/board/service/dto/MultiBoardSelectResponse.java
+++ src/main/java/com/project/kodesalon/model/board/service/dto/MultiBoardSelectResponse.java
@@ -10,21 +10,21 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class MultiBoardSelectResponse {
-    private List<BoardSelectResponse> boards;
+public class MultiBoardSelectResponse<E> {
+    private List<E> boards;
     private boolean isLast;
 
-    public MultiBoardSelectResponse(final List<BoardSelectResponse> boards, final int size) {
+    public MultiBoardSelectResponse(final List<E> boards, final int size) {
         this.isLast = hasLast(boards, size);
         this.boards = removeLastIfOverloaded(boards);
     }
 
-    private boolean hasLast(final List<BoardSelectResponse> boards, final int size) {
+    private boolean hasLast(final List<E> boards, final int size) {
         return boards.size() <= size;
     }
 
-    private List<BoardSelectResponse> removeLastIfOverloaded(final List<BoardSelectResponse> boards) {
-        LinkedList<BoardSelectResponse> boardSelectResponses = new LinkedList<>(boards);
+    private List<E> removeLastIfOverloaded(final List<E> boards) {
+        LinkedList<E> boardSelectResponses = new LinkedList<>(boards);
 
         if (!isLast) {
             boardSelectResponses.removeLast();
