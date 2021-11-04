ec4e4ea7510ed00cd12a8606db145de9ef0c9a91
Beenie93 <seongbeen93@naver.com>
2021-11-05 02:19:57 +0900

    refactor : #89 모든 profile 받아올 수 있도록 수정

--- src/main/java/com/project/kodesalon/controller/ProfileController.java
+++ src/main/java/com/project/kodesalon/controller/ProfileController.java
@@ -5,8 +5,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 @RestController
 public class ProfileController {
 
@@ -16,10 +14,8 @@ public class ProfileController {
         this.env = env;
     }
 
-    @GetMapping("/profile")
+    @GetMapping("/profiles")
     public String getProfile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .findFirst()
-                .orElse("");
+        return String.join(", ", env.getActiveProfiles());
     }
 }
