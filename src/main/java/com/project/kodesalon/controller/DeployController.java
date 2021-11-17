627a8d1eff94a5e77b7019deff5260124c506e71
sdrs6921 <sdrs6921@naver.com>
2021-11-17 17:38:21 +0900

    refactor : #44 DeployController 이름 변경 및 구현

--- src/main/java/com/project/kodesalon/controller/DeployController.java
+++ src/main/java/com/project/kodesalon/controller/DeployController.java
@@ -6,16 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProfileController {
+public class DeployController {
 
     private Environment env;
 
-    public ProfileController(final Environment env) {
+    public DeployController(final Environment env) {
         this.env = env;
     }
 
     @GetMapping("/profiles")
     public String getProfile() {
         return String.join(", ", env.getActiveProfiles());
+    }
+
+    @GetMapping("/health")
+    public String getHealth() {
+        return "UP";
     }
 }
